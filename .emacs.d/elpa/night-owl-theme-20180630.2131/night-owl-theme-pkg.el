f0a6dc16158c7a35cb73320ded8532dd403fb150
babariviere <babathriviere@gmail.com>
2019-04-29 16:09:54 +0200

    - add .emacs.d/backups - add .emacs.d/var - add .emacs.d/auto-save-list - add .emacs.d/elpa

--- .emacs.d/elpa/night-owl-theme-20180630.2131/night-owl-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "night-owl-theme" "20180630.2131" "A color theme for the night owls out there" '((emacs "24")) :commit "6c73732986015c6a36919f16096d5e4f81b516ad" :authors '(("Aaron Jensen" . "aaronjensen@gmail.com")) :maintainer '("Aaron Jensen" . "aaronjensen@gmail.com") :url "http://github.com/aaronjensen/night-owl-theme")
