635594da5034cc18eea31451225c3f3ee9c56ba0
babariviere <babathriviere@gmail.com>
2019-05-01 18:49:04 +0200

    - mod .emacs.d/init.el - add .emacs.d/bookmarks - mod .emacs.d/org.el - mod .emacs.d/recentf

--- .emacs.d/org.el
+++ .emacs.d/org.el
@@ -4,22 +4,49 @@
   (leader-define-key
    :states 'normal
    :keymaps 'org-mode-map
-   "t" 'org-todo)
+   "t" 'org-todo
+   "S-h" 'org-shiftleft
+   "S-l" 'org-shiftright)
   (leader-define-key
     :states 'normal
     "ol" 'org-store-link
     "oa" 'org-agenda
     "oc" 'org-capture)
   (setq org-log-done t)
-  (setq org-todo-keywords
-	'((sequence "TODO" "WORKING" "REVIEW" "DONE")))
   (setq org-src-fontify-natively t)
   (setq org-src-window-setup 'current-window)
-  ;;(setq org-todo-keyword-faces
-  ;;	'(("TODO" . "blue")
-  ;;	  ("WORKING" . "yellow")
-  ;;	  ("REVIEW" . "red")
-  ;;	  ("DONE" . "green")))
+
+  (setq org-directory (expand-file-name "~/org"))
+  (setq org-default-notes-file (concat org-directory "/notes.org"))
+  (setq org-agenda-files '(org-directory))
+
+  (setq org-agenda-skip-deadline-if-done t)
+  (setq org-agenda-skip-scheduled-if-done t)
+
+  ;; Keywords
+  (setq org-todo-keywords
+	'(
+	  (sequence "IDEA(i)" "TODO(t)" "STARTED(s)" "NEXT(n)" "WAITING(w)" "|" "DONE(d)")
+	  (sequence "|" "CANCELED(c)" "DELEGATED(l)" "SOMEDAY(f)")
+	  ))
+  (setq org-todo-keyword-faces
+	'(("IDEA" . (:foreground "GoldenRod" :weight bold))
+	  ("NEXT" . (:foreground "IndianRed1" :weight bold))   
+	  ("STARTED" . (:foreground "OrangeRed" :weight bold))
+	  ("WAITING" . (:foreground "coral" :weight bold)) 
+	  ("CANCELED" . (:foreground "LimeGreen" :weight bold))
+	  ("DELEGATED" . (:foreground "LimeGreen" :weight bold))
+	  ("SOMEDAY" . (:foreground "LimeGreen" :weight bold))
+	  ))
+
+  ;; Capture templates
+  (setq org-capture-templates
+	'(("t"
+	   "TODO"
+	   entry
+	   (file+headline org-default-notes-file "Tasks")
+	   "* TODO %?\n  Added: %U\n  From: %a" :prepend t :kill-buffer t)
+	  ))
   )
 
 (use-package evil-org
