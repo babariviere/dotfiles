fc0196637b79b1e14c8c3f00bc915fcf5725fe1a
babariviere <babathriviere@gmail.com>
2019-05-02 14:20:49 +0200

    - mod coc/extensions/package.json/ - add .emacs.d/eshell - mod .emacs.d/init.el - mod .emacs.d/lsp.el

--- .emacs.d/lsp.el
+++ .emacs.d/lsp.el
@@ -9,7 +9,10 @@
   (setq lsp-ui-doc-enable t
         lsp-ui-sideline-enable t
         lsp-ui-peek-enable t
-	lsp-ui-flycheck-enable t)
+	lsp-ui-flycheck-enable t
+	lsp-ui-doc-header t
+	lsp-ui-doc-include-signature t
+	lsp-ui-doc-use-childframe nil)
   (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
   (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
   (add-hook 'lsp-ui-doc-frame-hook
