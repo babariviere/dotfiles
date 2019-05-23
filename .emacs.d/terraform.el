05ba0b51f394caf6fda28b07978ecf85249a2127
babariviere <babathriviere@gmail.com>
2019-05-23 12:38:06 +0200

    - mod .emacs.d/terraform.el

--- .emacs.d/terraform.el
+++ .emacs.d/terraform.el
@@ -1,6 +1,13 @@
 (use-package terraform-mode
+  :after lsp-mode
+  :init
+  (add-hook 'terraform-mode-hook #'terraform-format-on-save-mode)
+  ;; (add-hook 'terraform-mode-hook #'lsp)
   :config
-  (add-hook 'terraform-mode-hook #'terraform-format-on-save-mode))
+  (lsp-register-client
+   (make-lsp-client :new-connection (lsp-stdio-connection "terraform-lsp")
+					:major-modes '(terraform-mode)
+					:server-id 'terraform-lsp)))
 
 (use-package company-terraform
   :after terraform-mode
