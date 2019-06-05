ec6a22f779e321fde62c09c162e3fe4b98b69a5d
babariviere <babathriviere@gmail.com>
2019-06-05 15:29:14 +0200

    - mod .emacs.d/init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -14,15 +14,15 @@
 ;; Set backup directory
 (defvar --backup-directory (concat user-emacs-directory "backups"))
 (if (not (file-exists-p --backup-directory))
-        (make-directory --backup-directory t))
+	(make-directory --backup-directory t))
 (setq backup-directory-alist `(("." . , --backup-directory)))
 
 ;; Set autosave directory
 (defvar --autosave-directory (concat user-emacs-directory "autosaves/"))
 (if (not (file-exists-p --autosave-directory))
-    (make-directory --autosave-directory t))
+	(make-directory --autosave-directory t))
 (setq auto-save-file-name-transforms
-      `((".*" , --autosave-directory t)))
+	  `((".*" , --autosave-directory t)))
 
 
 ;; Install use-package
@@ -42,7 +42,8 @@
 (use-package auto-package-update)
 
 ;; Set tab width
-(setq-default tab-width 4)
+(setq-default tab-width 2)
+(setq indent-tabs-mode nil)
 
 ;; Pinentry in emacs
 (use-package pinentry
@@ -60,25 +61,25 @@
 
 (defvar config-files
   '("private.el"
-    "ui.el" "keys.el"
-    "check.el" "completion.el"
-    "snippets.el" "direnv.el"
-    "navigation.el"
-    "org.el" "git.el"
+	"ui.el" "keys.el"
+	"check.el" "completion.el"
+	"snippets.el" "direnv.el"
+	"navigation.el"
+	"org.el" "git.el"
 
-    ;; Languages
-    "lsp.el"
-    "lisp.el"
-    "rust.el"
-    "go.el"
-    "markdown.el"
-    "fish.el"
-    "terraform.el"
-    "dart.el"
-    "yaml.el"
-    "feature.el"
-    "kotlin.el"
-    )
+	;; Languages
+	"lsp.el"
+	"lisp.el"
+	"rust.el"
+	"go.el"
+	"markdown.el"
+	"fish.el"
+	"terraform.el"
+	"dart.el"
+	"yaml.el"
+	"feature.el"
+	"kotlin.el"
+	)
   "A list of custom configuration file name.  Relative to 'user-emacs-directory'.")
 
 (dolist (c config-files)
@@ -92,11 +93,11 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("26d49386a2036df7ccbe802a06a759031e4455f07bda559dcf221f53e8850e69" "36ca8f60565af20ef4f30783aa16a26d96c02df7b4e54e9900a5138fb33808da" "59e82a683db7129c0142b4b5a35dbbeaf8e01a4b81588f8c163bd255b76f4d21" "8aca557e9a17174d8f847fb02870cb2bb67f3b6e808e46c0e54a44e3e18e1020" "d1b4990bd599f5e2186c3f75769a2c5334063e9e541e37514942c27975700370" "6d589ac0e52375d311afaa745205abb6ccb3b21f6ba037104d71111e7e76a3fc" "d2e9c7e31e574bf38f4b0fb927aaff20c1e5f92f72001102758005e53d77b8c9" "6b2636879127bf6124ce541b1b2824800afc49c6ccd65439d6eb987dbf200c36" "fd944f09d4d0c4d4a3c82bd7b3360f17e3ada8adf29f28199d09308ba01cc092" default)))
+	("26d49386a2036df7ccbe802a06a759031e4455f07bda559dcf221f53e8850e69" "36ca8f60565af20ef4f30783aa16a26d96c02df7b4e54e9900a5138fb33808da" "59e82a683db7129c0142b4b5a35dbbeaf8e01a4b81588f8c163bd255b76f4d21" "8aca557e9a17174d8f847fb02870cb2bb67f3b6e808e46c0e54a44e3e18e1020" "d1b4990bd599f5e2186c3f75769a2c5334063e9e541e37514942c27975700370" "6d589ac0e52375d311afaa745205abb6ccb3b21f6ba037104d71111e7e76a3fc" "d2e9c7e31e574bf38f4b0fb927aaff20c1e5f92f72001102758005e53d77b8c9" "6b2636879127bf6124ce541b1b2824800afc49c6ccd65439d6eb987dbf200c36" "fd944f09d4d0c4d4a3c82bd7b3360f17e3ada8adf29f28199d09308ba01cc092" default)))
  '(ivy-virtual-abbreviate (quote full))
  '(package-selected-packages
    (quote
-    (feature-mode go-add-tags gotest go-guru godoctor dart-mode company-terraform terraform-mode fish-mode go-mode cargo rust-mode toml-mode company-lsp lsp-ui lsp-mode forge ghub git-gutter evil-magit magit org-jira org-bullets evil-org counsel-projectile projectile ivy-rich ivy direnv yasnippet company-quickhelp company flycheck evil-surround evil general nyan-mode which-key rainbow-delimiters doom-modeline doom-themes auto-package-update auto-compile use-package))))
+	(feature-mode go-add-tags gotest go-guru godoctor dart-mode company-terraform terraform-mode fish-mode go-mode cargo rust-mode toml-mode company-lsp lsp-ui lsp-mode forge ghub git-gutter evil-magit magit org-jira org-bullets evil-org counsel-projectile projectile ivy-rich ivy direnv yasnippet company-quickhelp company flycheck evil-surround evil general nyan-mode which-key rainbow-delimiters doom-modeline doom-themes auto-package-update auto-compile use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
