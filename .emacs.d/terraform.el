d9d1d3b210add59eacc0b1bb33663fe75ebf925d
babariviere <babathriviere@gmail.com>
2019-06-06 15:23:57 +0200

    - mod .emacs.d/terraform.el

--- .emacs.d/terraform.el
+++ .emacs.d/terraform.el
@@ -1,24 +1,7 @@
-(defun indent-buffer ()
-  (interactive)
-  (save-excursion
-	(indent-region (point-min) (point-max) nil)))
-
-
-(define-minor-mode terraform-format-on-save-fix-mode
-  "Format file on save + apply indent (because breaked in 0.12)"
-  :lighter ""
-  (if terraform-format-on-save-fix-mode
-	  (add-hook 'before-save-hook #'terraform-format-fix nil t)
-	(remove-hook 'before-save-hook #'terraform-format-fix t)))
-
-(defun terraform-format-fix ()
-  (terraform-format-buffer)
-  (indent-buffer))
-
 (use-package terraform-mode
   :after lsp-mode
   :init
-  (add-hook 'terraform-mode-hook #'terraform-format-on-save-fix-mode)
+  (add-hook 'terraform-mode-hook #'terraform-format-on-save-mode)
   ;; (add-hook 'terraform-mode-hook #'lsp)
   :config
   (lsp-register-client
