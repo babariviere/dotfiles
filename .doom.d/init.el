9d797f4569538bf87242315f5ebdcaee9596c575
babariviere <babathriviere@gmail.com>
2019-08-26 20:23:19 +0200

    - mod .doom.d/init.el

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -16,7 +16,9 @@
        company           ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       ivy               ; a search engine for love and life
+       (ivy
+        +fuzzy
+        +icons)            ; a search engine for love and life
 
        :ui
        ;;deft              ; notational velocity for Emacs
