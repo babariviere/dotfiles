4a59284647448dee4355808832bf49b9c29e1309
babariviere <babathriviere@gmail.com>
2019-08-29 14:30:41 +0200

    - mod .doom.d/init.el

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -147,8 +147,7 @@
        ;;solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
-       (web
-        +javascript)      ; the tubes
+       web                 ; the tubes
        ;;vala              ; GObjective-C
 
        :email
