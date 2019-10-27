8c00b9bb21eb8598963be24faa886da75a9cff00
Bastien Rivière <babathriviere@gmail.com>
2019-10-27 19:50:18 +0100

    add system version + upgrade

--- default.nix
+++ default.nix
@@ -47,4 +47,15 @@
   services.dotfiles.name = "Bastien Rivière";
 
   time.timeZone = "Europe/Paris";
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "19.09"; # Did you read the comment?
+  system.autoUpgrade = {
+    enable = true;
+    channel = "https://nixos.org/channels/nixos-19.09";
+    # TODO: set date ?
+  };
 }
