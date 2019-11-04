7294423bd80d9089078fa7573889f15f59ce1a1f
Bastien Rivière <babathriviere@gmail.com>
2019-11-04 09:34:38 +0100

    update insomnia to unstable

--- modules/tools/insomnia.nix
+++ modules/tools/insomnia.nix
@@ -1,8 +1,9 @@
 { config, lib, pkgs, ... }:
 
-let cfg = config.dotfiles.tools.insomnia;
+let
+  cfg = config.dotfiles.tools.insomnia;
+  unstable = import <nixpkgs-unstable> { };
 in {
-  config = lib.mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [ insomnia ];
-  };
+  config =
+    lib.mkIf cfg.enable { environment.systemPackages = [ unstable.insomnia ]; };
 }
