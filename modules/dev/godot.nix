e072252df57f9c5ae58699c537dfcda91d45499d
Bastien Rivière <babathriviere@gmail.com>
2020-01-08 21:24:13 +0100

    use godot from my nur repo

--- modules/dev/godot.nix
+++ modules/dev/godot.nix
@@ -3,6 +3,7 @@
 let cfg = config.dotfiles;
 in {
   config = lib.mkIf cfg.dev.godot.enable {
-    environment.systemPackages = with pkgs; [ godot ];
+    environment.systemPackages = with pkgs;
+      [ pkgs.nur.repos.babariviere.godot ];
   };
 }
