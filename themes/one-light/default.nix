83936c86bfaae1657f7dced7e113648a9ef27e4a
Bastien Rivière <babathriviere@gmail.com>
2020-01-09 12:24:16 +0100

    add one themes (light version needs rework)

---
+++ themes/one-light/default.nix
@@ -0,0 +1,25 @@
+{
+  # Special
+  foreground = "#2a2b32";
+  foregroundBold = "#2a2b32";
+  cursor = "#2a2b32";
+  background = "#f8f8f8";
+
+  # colors
+  black = "#000000";
+  red = "#da3e39";
+  green = "#41933e";
+  yellow = "#855504";
+  blue = "#315eee";
+  magenta = "#930092";
+  cyan = "#0e6fad";
+  white = "#8e8f96";
+  lightBlack = "#2a2b32";
+  lightRed = "#da3e39";
+  lightGreen = "#41933e";
+  lightYellow = "#855504";
+  lightBlue = "#315eee";
+  lightMagenta = "#930092";
+  lightCyan = "#0e6fad";
+  lightWhite = "#fffefe";
+}
---
+++ themes/one/default.nix
@@ -0,0 +1,25 @@
+{
+  # Special
+  foreground = "#abb2bf";
+  foregroundBold = "#abb2bf";
+  cursor = "#abb2bf";
+  background = "#282c34";
+
+  # colors
+  black = "#282c34";
+  red = "#e06c75";
+  green = "#98c379";
+  yellow = "#e5c07b";
+  blue = "#61afef";
+  magenta = "#c678dd";
+  cyan = "#56b6c2";
+  white = "#abb2bf";
+  lightBlack = "#5c6370";
+  lightRed = "#be5046";
+  lightGreen = "#7a9f60";
+  lightYellow = "#d19a66";
+  lightBlue = "#3b84c0";
+  lightMagenta = "#9a52af";
+  lightCyan = "#3c909b";
+  lightWhite = "#828997";
+}
