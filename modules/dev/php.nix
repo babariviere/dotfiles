7a5f75058c7dc93186745e4caf94490fd75d33b1
Bastien Rivière <babathriviere@gmail.com>
2020-01-30 10:38:25 +0100

    add phpactor + instructions

--- modules/dev/php.nix
+++ modules/dev/php.nix
@@ -14,6 +14,7 @@ in {
     home-manager.users."${dotfiles.user}" = {
       xdg.configFile = {
         "zsh/rc.d/env.php.zsh".source = <config/php/env.zsh>;
+        # To add a package, go to config/php/composer and do `composer require <package>`
         "composer/composer.json".source = <config/php/composer/composer.json>;
       };
     };
