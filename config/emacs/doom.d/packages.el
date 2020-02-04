45d5fce81d8b963588050df64955875ff262a6af
Bastien Rivière <babathriviere@gmail.com>
2020-02-04 12:17:07 +0100

    pin packages

--- config/emacs/doom.d/packages.el
+++ config/emacs/doom.d/packages.el
@@ -6,7 +6,7 @@
 ;; (package! another-package :recipe (:host github :repo "username/repo"))
 ;; (package! builtin-package :disable t)
 
-(package! systemd)
-(package! dart-mode)
-(package! org-fancy-priorities)
-(package! graphql)
+(package! systemd :pin "51c148e09a")
+(package! dart-mode :pin "04fcd649f1")
+(package! org-fancy-priorities :pin "819bb993b7")
+(package! graphql :pin "e2b309689f")
