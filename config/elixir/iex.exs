424d49b640d9a89b2684ffee77b12c18bc65ee80
Bastien Rivière <babathriviere@gmail.com>
2020-04-11 04:40:25 +0200

    helper to expand macros

--- config/elixir/iex.exs
+++ config/elixir/iex.exs
@@ -6,3 +6,14 @@ IEx.configure(
     limit: 5
   ]
 )
+
+defmodule Helpers do
+  def expand(macro) do
+    macro
+    |> Macro.expand(__ENV__)
+    |> Macro.to_string()
+    |> IO.puts()
+  end
+end
+
+import Helpers
