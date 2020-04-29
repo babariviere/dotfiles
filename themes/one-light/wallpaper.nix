7f2bcfe9fbdf65a2d81b80c5ae88d54c9a09a660
Bastien Rivière <babathriviere@gmail.com>
2020-04-29 09:38:50 +0200

    change wallpaper for one-light

--- themes/one-light/wallpaper.nix
+++ themes/one-light/wallpaper.nix
@@ -2,12 +2,9 @@
 
 {
   src = fetchurl {
-    url =
-      "https://images.wallpapersden.com/image/download/moon-space-minimal_61440_1920x1080.jpg";
-    sha256 = "1fwflzgkd4gbsj12v3vfpghnx0p12jq2vvizdvwk1afbx57667sv";
+    url = "https://unsplash.com/photos/_f2m3mEkaaU/download?force=true";
+    sha256 = "08zgkh8d20gq4xvd49y129f9ba5vgwwq9bcsx8gzwr4agd8kkz4p";
   };
 
-  meta = {
-    src = "https://wallpapersden.com/moon-space-minimal-wallpaper/1920x1080";
-  };
+  meta = { src = "https://unsplash.com/photos/_f2m3mEkaaU"; };
 }
