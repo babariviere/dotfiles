f5d92cdff536c7d5463de8a39eea6a745be0eba3
Bastien Rivière <babathriviere@gmail.com>
2020-05-31 20:30:45 +0200

    Update readme and delete old scripts

--- switch.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [ -z "$1" ]; then
-    echo "Usage: $0 <machine>"
-    exit 1
-fi
-
-nix build --no-link -f "<nixpkgs/nixos>" -I "nixos-config=devices/$1.nix" -I "config=./config" config.system.build.toplevel
-nixos-rebuild -I "nixos-config=devices/$1.nix" -I "config=./config" "${@:2}"
