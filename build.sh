97e75ce437833e43d9b41d9aa806b79be0db5c64
Bastien Rivière <babathriviere@gmail.com>
2020-05-31 20:30:45 +0200

    Update readme and delete old scripts

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ -z "$1" ]; then
-    echo "Usage: $0 <machine>"
-    exit 1
-fi
-nix-build "<nixpkgs/nixos>" -A vm -k -I "nixos-config=devices/$1.nix" -I "config=./config" "${@:2}"
