83a514bd698d6dcd8954386e401f055f602f9ce3
Bastien Rivière <babathriviere@gmail.com>
2020-06-13 21:51:02 +0200

    yarn: export .yarn/bin to PATH

--- modules/dev/javascript.nix
+++ modules/dev/javascript.nix
@@ -1,12 +1,20 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, usrconf, ... }:
 
-let cfg = config.dotfiles;
+let
+  dotfiles = config.dotfiles;
+  cfg = dotfiles.dev.javascript;
 in {
   options.dotfiles.dev.javascript.enable = lib.mkEnableOption "javascript";
 
-  config = lib.mkIf cfg.dev.javascript.enable {
+  config = lib.mkIf cfg.enable {
     environment.systemPackages = with pkgs;
       [ nodejs yarn ]
       ++ (with nodePackages; [ typescript typescript-language-server ]);
+
+    home-manager.users."${dotfiles.user}" = {
+      xdg.configFile = {
+        "zsh/rc.d/env.yarn.zsh".source = (usrconf "yarn/env.zsh");
+      };
+    };
   };
 }
