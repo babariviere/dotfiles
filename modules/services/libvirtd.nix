6b4046db3c48b2eedb081bb62c74d66ceb885413
Bastien Rivière <babathriviere@gmail.com>
2020-06-17 22:22:54 +0200

    kvm: add modprobe config

--- modules/services/libvirtd.nix
+++ modules/services/libvirtd.nix
@@ -15,5 +15,11 @@ in {
     environment.systemPackages = with pkgs; [ virtmanager ];
 
     users.extraGroups.libvirtd.members = [ dotfiles.user ];
+
+    boot.extraModprobeConfig = ''
+      options kvm_intel nested=1
+      options kvm_intel emulate_invalid_guest_state=0
+      options kvm ignore_msrs=1
+    '';
   };
 }
