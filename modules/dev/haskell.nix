6d075496d20cf5ccfeb543940aac7a69bc5ab930
Bastien Rivière <babathriviere@gmail.com>
2020-06-18 12:11:02 +0200

    haskell: disable ghcide

--- modules/dev/haskell.nix
+++ modules/dev/haskell.nix
@@ -3,18 +3,14 @@
 let
   dotfiles = config.dotfiles;
   cfg = dotfiles.dev.haskell;
+
+  ghc' =
+    pkgs.haskellPackages.ghcWithPackages (ps: with ps; [ hoogle ormolu hlint ]);
 in {
   options.dotfiles.dev.haskell.enable = lib.mkEnableOption "haskell";
 
   config = lib.mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [
-      # cabal-install
-      stack
-      # snack
-      # cabal2nix
-      (haskellPackages.ghcWithPackages
-        (ps: with ps; [ hoogle ormolu hlint ghcide ]))
-    ];
+    environment.systemPackages = with pkgs; [ stack ghc' ];
 
     home-manager.users."${dotfiles.user}" = {
       home.file.".ghci".source = pkgs.mutate (usrconf "ghci") {
