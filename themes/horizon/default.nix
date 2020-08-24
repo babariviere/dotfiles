68a3d6924923b33f28fa177d4088efa2b8b2fb30
Bastien Rivière <babathriviere@gmail.com>
2020-08-24 20:13:04 +0200

    style(horizon): fix some colors

--- themes/horizon/default.nix
+++ themes/horizon/default.nix
@@ -8,19 +8,19 @@ rec {
 
   # colors
   black = "#1c1e26";
-  red = "#e95378";
+  red = "#e95678";
   green = "#29d398";
   yellow = "#fab795";
   blue = "#26bbd9";
-  magenta = "#ee64ac";
-  cyan = "#59e1e3";
+  magenta = "#ee64ae";
+  cyan = "#59e3e3";
   white = "#d5d8da";
   lightBlack = "#2e303e";
   lightRed = "#ec6a88";
   lightGreen = "#3fdaa4";
   lightYellow = "#fbc3a7";
-  lightBlue = "#3fc4de";
-  lightMagenta = "#f075b5";
-  lightCyan = "#6be4e6";
+  lightBlue = "#3fc6de";
+  lightMagenta = "#f075b7";
+  lightCyan = "#6be6e6";
   lightWhite = "#ffffff";
 }
