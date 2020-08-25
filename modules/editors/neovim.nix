4792cba473c46f6519a20ec77360845649d63a06
Bastien Rivière <babathriviere@gmail.com>
2020-08-25 23:42:26 +0200

    feat(neovim): add ranger

--- modules/editors/neovim.nix
+++ modules/editors/neovim.nix
@@ -5,6 +5,7 @@ in {
   options.dotfiles.editors.neovim.enable = lib.mkEnableOption "neovim";
 
   config = lib.mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [ neovim xsel ];
+    # TODO: install ranger as another module
+    environment.systemPackages = with pkgs; [ neovim xsel ranger ueberzug ];
   };
 }
