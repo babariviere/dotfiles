5cf5fcfc10e9f2b7e11ab5771358d990608c84a1
Bastien Rivière <babathriviere@gmail.com>
2020-09-09 10:30:58 +0200

    feat(zsh): add more aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,3 +3,11 @@ alias ll="ls -al"
 
 alias v="nvim"
 alias vim="nvim"
+
+alias gch="git checkout"
+alias gb="git branch"
+alias gpl="git pull"
+alias gps="git push"
+
+alias dup="docker-compose up"
+alias ddn="docker-compose down"
