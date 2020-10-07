55e0cbfd9e1e124ad550e454d79fe314ebd993a0
Bastien Rivière <babathriviere@gmail.com>
2020-10-07 20:24:24 +0200

    feat(update): add update of plugins to latest version

--- _system/common/asdf.sh
+++ _system/common/asdf.sh
@@ -2,3 +2,10 @@
 
 asdf update
 asdf plugin-update --all
+
+# Update packages to latest version
+for pkg in $(asdf plugin list); do
+  latest=$(asdf latest "$pkg")
+  asdf install "$pkg" "$latest"
+  asdf global "$pkg" "$latest"
+done
