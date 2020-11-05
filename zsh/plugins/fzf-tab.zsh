41dac1f05ef0ea1d20d0e18f727a1bc005f065b9
Bastien Rivière <babathriviere@gmail.com>
2020-11-05 12:14:19 +0100

    feat(zsh): fzf-tab use popup now

--- zsh/plugins/fzf-tab.zsh
+++ zsh/plugins/fzf-tab.zsh
@@ -1,5 +1,11 @@
 enable-fzf-tab
 
+if [ -n "$TMUX" ]; then
+  # Use popup
+  zstyle ':fzf-tab:*' fzf-command ftb-tmux-popup
+  zstyle ':fzf-tab:*' popup-pad 50 20
+fi
+
 zstyle ":completion:*:git-checkout:*" sort false
 zstyle ':completion:*:descriptions' format '[%d]'
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
