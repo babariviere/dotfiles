d43b89f703393e4e4889c7f8b54ac37a20177f57
Bastien Rivière <babathriviere@gmail.com>
2020-12-09 12:38:53 +0100

    chore(nvim): change signify signs

--- config/nvim/lua/baba/plugins/signify.lua
+++ config/nvim/lua/baba/plugins/signify.lua
@@ -1,8 +1,8 @@
 -- Change these if you want
-vim.g.signify_sign_add = '+'
-vim.g.signify_sign_delete = '_'
-vim.g.signify_sign_delete_first_line = '‾'
-vim.g.signify_sign_change = '~'
+vim.g.signify_sign_add = '▎'
+vim.g.signify_sign_delete = '▁'
+vim.g.signify_sign_delete_first_line = '▔'
+vim.g.signify_sign_change = '▎'
 
 -- I find the numbers disctracting
 vim.g.signify_sign_show_count = 0
--- config/nvim/lua/baba/themes/dracula.lua
+++ config/nvim/lua/baba/themes/dracula.lua
@@ -1,6 +1,8 @@
 vim.o.background = 'dark'
 
-vim.g.lightline = { colorschem = 'dracula' }
+vim.g.lightline = {colorschem = 'dracula'}
 vim.g.airline_theme = 'dracula'
 
 vim.api.nvim_command [[colorscheme dracula]]
+
+vim.api.nvim_command [[highlight DraculaDiffDelete guibg=none ctermbg=none]]
