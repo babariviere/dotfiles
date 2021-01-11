5b96085d03c262c2c6ebd52d52ee80fe9a64b59f
Bastien Rivière <babathriviere@gmail.com>
2021-01-11 15:01:44 +0100

    feat(hammerspoon): add semi-auto tiling

--- hammerspoon/lib/bsp.lua
+++ hammerspoon/lib/bsp.lua
@@ -1,12 +1,15 @@
 local Node = {}
 Node.__index = Node
 
+-- TODO: better way to modify nodes without forgetting values (e.g. id)
+
 function Node.new(rect, ratio)
   local node = {
     firstChild = nil,
     secondChild = nil,
     parent = nil,
     window = nil, -- only set when window is leaf
+    id = nil,
     rect = rect:floor(),
     ratio = ratio or 0.5
   }
@@ -18,6 +21,7 @@ function Node:isRoot() return self.parent == nil end
 
 function Node:isLeaf() return self.firstChild == nil and self.secondChild == nil end
 
+-- TODO: rename me into insertLeaf
 function Node:insertWindow(window)
   -- TODO: when is leaf, split and create new node
   -- when not leaf, find biggest rect
@@ -29,6 +33,7 @@ function Node:insertWindow(window)
 
   if self.window == nil then
     self.window = window
+    self.id = window:id()
     return
   end
 
@@ -48,14 +53,42 @@ function Node:insertWindow(window)
   local first = Node.new(firstRect, 0.5) -- TODO: configurable ratio
   first.parent = self
   first.window = self.window
+  first.id = self.id
   self.window = nil
+  self.id = nil
 
   local second = Node.new(secondRect, 0.5)
   second.parent = self
   second.window = window
+  second.id = window:id()
 
   self.firstChild = first
   self.secondChild = second
+end
+
+function Node:deleteWindow(window)
+  local leaf = self:findLeaf(window:id())
+  if not leaf then return end
+
+  if not leaf.parent then
+    leaf.window = nil
+    leaf.id = nil
+    return
+  end
+
+  local parent = leaf.parent
+
+  if parent.firstChild == self then
+    parent.window = parent.secondChild.window
+    parent.id = parent.secondChild.id
+    parent.firstChild = nil
+    parent.secondChild = nil
+  else
+    parent.window = parent.firstChild.window
+    parent.id = parent.firstChild.id
+    parent.firstChild = nil
+    parent.secondChild = nil
+  end
 end
 
 -- Return biggest leaf
@@ -71,6 +104,20 @@ function Node:biggestLeaf()
   end
 
   return self
+end
+
+-- Find leaf in tree by it's id
+function Node:findLeaf(leaf)
+  if not self:isLeaf() then
+    local first = self.firstChild:findLeaf(leaf)
+    if first then return first end
+    local second = self.secondChild:findLeaf(leaf)
+    if second then return second end
+  end
+
+  if self.id == leaf then return self end
+
+  return nil
 end
 
 -- Execute function for each leaf
--- hammerspoon/window/tile.lua
+++ hammerspoon/window/tile.lua
@@ -1,27 +1,67 @@
-local M = {}
+local module = {
+  -- Windows in spaces
+  spaces = {}
+}
 
 local bsp = require('lib/bsp')
+local spaces = require('hs._asm.undocumented.spaces')
+local wf = hs.window.filter
 
-function M.tile()
+local function moveWindow()
+  local delay = 0.
+
+  return function(node)
+    if node.rect == node.window:frame() then return end
+    hs.timer.doAfter(delay, function() node.window:setFrame(node.rect) end)
+    delay = delay + 0.2
+  end
+end
+
+local function onWindowCreated(window, appName, event)
+  local space = window:spaces()[1]
+
+  local root = module.spaces[space]
+  if not root then return end
+
+  root:insertWindow(window)
+
+  root:forEachLeaf(moveWindow())
+end
+
+local function onWindowDestroyed(window, appName, evnet)
+  local space = spaces.activeSpace()
+
+  local root = module.spaces[space]
+  if not root then return end
+
+  root:deleteWindow(window)
+
+  root:forEachLeaf(moveWindow())
+end
+
+function module.tile()
   local screen = hs.screen.mainScreen()
-  local filter = hs.window.filter.new():setCurrentSpace(true):setScreens(
-                   {screen:id()}):rejectApp('Notification Centre')
+  local filter = wf.new():setCurrentSpace(true):setScreens({screen:id()})
+                   :rejectApp('Notification Centre')
   local windows = filter:getWindows()
 
   local root = bsp.new(screen:frame()) -- root is the screen, it should not be resized
 
   for _, window in pairs(windows) do root:insertWindow(window) end
 
-  local delay = 0.
-  root:forEachLeaf(function(node)
-    if node.rect == node.window:frame() then return end
-    hs.timer.doAfter(delay, function() node.window:setFrame(node.rect) end)
-    delay = delay + 0.2
-  end)
+  root:forEachLeaf(moveWindow())
+
+  module.spaces[spaces.activeSpace()] = root
 end
 
-function M.start() hs.hotkey.bind({'cmd', 'alt'}, 'k', 'Tile', M.tile) end
+function module.start()
+  hs.hotkey.bind({'cmd', 'alt'}, 'k', 'Tile', module.tile)
 
-function M.stop() end
+  module._onCreate = wf.new():subscribe(wf.windowCreated, onWindowCreated)
+  module._onDestroy = wf.new():subscribe(wf.windowDestroyed, onWindowDestroyed)
+                        :subscribe(wf.windowHidden, onWindowDestroyed)
+end
 
-return M
+function module.stop() module._onCreate:unsubscribeAll() end
+
+return module
