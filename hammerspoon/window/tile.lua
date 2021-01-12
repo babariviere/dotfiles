d275825dffab406bfb8c4f2d2c696919b6bb29f6
Bastien Rivière <babathriviere@gmail.com>
2021-01-12 14:07:16 +0100

    feat(hammerspoon): add indicator for tiling

--- hammerspoon/window/tile.lua
+++ hammerspoon/window/tile.lua
@@ -1,11 +1,16 @@
 local module = {
   -- Windows in spaces
-  spaces = {}
+  spaces = {},
+  assets = {},
+
+  --- Options
+  menubar = true
 }
 
 local bsp = require('lib/bsp')
 local spaces = require('hs._asm.undocumented.spaces')
 local wf = require('hs.window.filter')
+local mb = require('hs.menubar')
 
 local function moveWindow()
   local delay = 0.
@@ -42,6 +47,27 @@ local function onWindowDestroyed(window, _, _)
   root:forEachLeaf(moveWindow())
 end
 
+local function onWindowFocused(window, _, _)
+  local space = window:spaces()[1]
+  if module.spaces[space] then
+    module._menubar:setIcon(module.assets.active)
+  else
+    module._menubar:setIcon(module.assets.inactive)
+  end
+end
+
+function module.toggleTile()
+  local space = spaces.activeSpace()
+  if module.spaces[space] then
+    hs.alert('No Tile') -- TODO(babariviere): better text
+    module.spaces[space] = nil
+    module._menubar:setIcon(module.assets.inactive)
+  else
+    hs.alert('Tile')
+    module.tile()
+  end
+end
+
 function module.tile()
   local screen = hs.screen.mainScreen()
   local filter = wf.new():setCurrentSpace(true):setScreens({screen:id()}):rejectApp('Notification Centre')
@@ -54,17 +80,40 @@ function module.tile()
   root:forEachLeaf(moveWindow())
 
   module.spaces[spaces.activeSpace()] = root
+  module._menubar:setIcon(module.assets.active)
+end
+
+function module.setupAssets()
+  module.assets = {}
+  module.assets.active = hs.image.imageFromPath(hs.configdir .. '/assets/tile_active.png'):size({w = 16, h = 16})
+  module.assets.inactive = hs.image.imageFromPath(hs.configdir .. '/assets/tile_inactive.png'):size({w = 16, h = 16})
+end
+
+-- TODO(babariviere): on space focus, change menubar icon
+function module.createMenubar()
+  module._menubar = mb.new()
+  module._menubar:setClickCallback(module.toggleTile)
+  module._menubar:setIcon(module.assets.inactive)
+  module._menubar:setTooltip('Toggle Tile')
 end
 
 function module.start()
-  hs.hotkey.bind({'cmd', 'alt'}, 'k', 'Tile', module.tile)
+  module.setupAssets()
 
-  module._onCreate = wf.new():subscribe(wf.windowCreated, onWindowCreated)
-  module._onDestroy = wf.new():subscribe({wf.windowDestroyed, wf.windowHidden, wf.windowMinimized}, onWindowDestroyed)
+  hs.hotkey.bind({'cmd', 'alt'}, 'k', 'Tile', module.tile)
+  hs.hotkey.bind({'cmd', 'alt'}, 'j', 'Toggle Tile', module.toggleTile)
+
+  module._wf = wf.new():subscribe(wf.windowCreated, onWindowCreated):subscribe(
+                 {wf.windowDestroyed, wf.windowHidden, wf.windowMinimized}, onWindowDestroyed):subscribe(
+                 {wf.windowFocused}, onWindowFocused)
+
+  if module.menubar then module.createMenubar() end
 end
 
 function module.stop()
-  module._onCreate:unsubscribeAll()
+  module._wf:unsubscribeAll()
+
+  if module._menubar then module._menubar:delete() end
 end
 
 return module
