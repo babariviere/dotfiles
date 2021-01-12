66c72b8d5398fa0d7e85526f664a805a74b16a78
Bastien Rivière <babathriviere@gmail.com>
2021-01-12 14:08:10 +0100

    feat(zsh): increase history size

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -27,8 +27,8 @@ unsetopt CHECK_JOBS       # Don't report on jobs when shell exit.
 
 ## History
 HISTFILE="$HOME/.cache/zhistory"
-HISTSIZE=1000                    # Max events to store in internal history.
-SAVEHIST=1000                    # Max events to store in history file.
+HISTSIZE=100000                  # Max events to store in internal history.
+SAVEHIST=100000                  # Max events to store in history file.
 
 setopt BANG_HIST                 # Don't treat '!' specially during expansion.
 setopt EXTENDED_HISTORY          # Write the history file in the ':start:elapsed;command' format.
