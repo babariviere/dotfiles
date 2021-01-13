06873617305ef8c72507c1ce8879c220b881e9bb
Bastien Rivière <babathriviere@gmail.com>
2021-01-13 12:47:46 +0100

    feat(nvim): add extensions to telescope

--- config/nvim/lua/baba/plugins.lua
+++ config/nvim/lua/baba/plugins.lua
@@ -2,8 +2,7 @@
 local packer_exists = pcall(vim.cmd, [[packadd packer.nvim]])
 
 if not packer_exists then
-  vim.cmd('!git clone https://github.com/wbthomason/packer.nvim ' ..
-            install_path)
+  vim.cmd('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
   vim.cmd 'packadd packer.nvim'
 end
 
@@ -60,7 +59,9 @@ return require('packer').startup {
 
     use {
       'nvim-treesitter/nvim-treesitter',
-      run = function() vim.api.nvim_command [[TSUpdate]] end,
+      run = function()
+        vim.api.nvim_command [[TSUpdate]]
+      end,
       config = [[require'baba.treesitter']]
     }
 
@@ -74,39 +75,30 @@ return require('packer').startup {
     use 'nvim-lua/lsp-status.nvim'
     use 'tjdevries/lsp_extensions.nvim'
     use 'RishabhRD/popfix'
-    use {
-      'RishabhRD/nvim-lsputils',
-      requires = {{'RishabhRD/popfix', opt = true}}
-    }
+    use {'RishabhRD/nvim-lsputils', requires = {{'RishabhRD/popfix', opt = true}}}
 
     -- DAP
-    use {
-      {'mfussenegger/nvim-dap', config = [[require'baba.dap']]},
-      'theHamsta/nvim-dap-virtual-text'
-    }
+    use {{'mfussenegger/nvim-dap', config = [[require'baba.dap']]}, 'theHamsta/nvim-dap-virtual-text'}
 
     -- Lua nvim
     use 'nvim-lua/plenary.nvim'
-    use {
-      'nvim-lua/popup.nvim',
-      requires = {{'nvim-lua/plenary.nvim', opt = true}}
-    }
+    use {'nvim-lua/popup.nvim', requires = {{'nvim-lua/plenary.nvim', opt = true}}}
     pkg {
       'nvim-telescope/telescope.nvim',
       as = 'telescope',
       requires = {
         {'nvim-lua/popup.nvim', opt = true},
-        {'nvim-lua/plenary.nvim', opt = true}
+        {'nvim-lua/plenary.nvim', opt = true},
+
+        -- extensions
+        {'nvim-telescope/telescope-fzy-native.nvim'},
+        {'nvim-telescope/telescope-github.nvim'}
       }
     }
     -- use 'rafcamlet/nvim-luapad'
 
     -- Flow
-    pkg {
-      'babariviere/flow.nvim',
-      as = 'flow',
-      requires = {'nvim-telescope/telescope.nvim', opt = true}
-    }
+    pkg {'babariviere/flow.nvim', as = 'flow', requires = {'nvim-telescope/telescope.nvim', opt = true}}
 
     -- Snippets
     -- use 'SirVer/ultisnips'
@@ -149,18 +141,15 @@ return require('packer').startup {
       -- {'mhinz/vim-signify', config = [[require'baba.git']]},
       {
         'lewis6991/gitsigns.nvim',
-        config = function() end,
+        config = function()
+        end,
         requires = {'nvim-lua/plenary.nvim'}
       },
       {'f-person/git-blame.nvim', config = [[require'baba.git']]}
     }
     use {
       'pwntester/octo.nvim',
-      requires = {
-        'nvim-lua/popup.nvim',
-        'nvim-lua/plenary.nvim',
-        'nvim-telescope/telescope.nvim'
-      }
+      requires = {'nvim-lua/popup.nvim', 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope.nvim'}
     }
 
     -- Start page
@@ -196,15 +185,13 @@ return require('packer').startup {
         -- Can only use plenary when we have our plugins.
         -- We can only get plenary when we don't have our plugins ;)
         local ok, float_win = pcall(function()
-          return require('plenary.window.float').percentage_range_window(0.8,
-                                                                         0.8)
+          return require('plenary.window.float').percentage_range_window(0.8, 0.8)
         end)
 
         if not ok then
           vim.cmd [[65vnew  [packer] ]]
 
-          return true, vim.api.nvim_get_current_win(),
-                 vim.api.nvim_get_current_buf()
+          return true, vim.api.nvim_get_current_win(), vim.api.nvim_get_current_buf()
         end
 
         local bufnr = float_win.bufnr
--- config/nvim/lua/baba/plugins/telescope.lua
+++ config/nvim/lua/baba/plugins/telescope.lua
@@ -1,7 +1,13 @@
-require'telescope'.setup {
-  defaults = require('telescope.themes').get_dropdown {
-    use_less = false,
-    width = 160,
-    results_height = 20
+local telescope = require 'telescope'
+local previewers = require 'telescope.previewers'
+
+telescope.setup {
+  defaults = {
+    file_previewer = previewers.vim_buffer_cat.new,
+    grep_previewer = previewers.vim_buffer_vimgrep.new,
+    qflist_previewer = previewers.vim_buffer_qflist.new
   }
 }
+
+telescope.load_extension('fzy_native')
+telescope.load_extension('gh')
