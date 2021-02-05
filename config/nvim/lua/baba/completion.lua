ff2d207e5a4ae6b0663b83f3d5b2452d4f00e7c9
Bastien Rivière <babathriviere@gmail.com>
2021-02-05 12:26:19 +0100

    feat(nvim): custom completion labels

--- config/nvim/lua/baba/completion.lua
+++ config/nvim/lua/baba/completion.lua
@@ -1,10 +1,10 @@
 vim.api.nvim_command [[autocmd BufEnter * lua require'completion'.on_attach{
-    enable_auto_hover = 1,
-    enable_auto_signature = 1
-  }]]
+  enable_auto_hover = 1,
+  enable_auto_signature = 1
+}]]
 
 vim.api.nvim_set_keymap("i", "<c-space>", "completion#trigger_completion()",
-                        {noremap = true, silent = true, expr = true})
+  {noremap = true, silent = true, expr = true})
 
 -- TODO(babariviere): only allow path in string or comments
 vim.g.completion_chain_complete_list = {
@@ -13,3 +13,21 @@ vim.g.completion_chain_complete_list = {
   {mode = "<c-p>"},
   {mode = "<c-n>"}
 }
+
+vim.g.completion_customize_lsp_label = {
+  Function = " [function]",
+  Method = " [method]",
+  Reference = " [refrence]",
+  Enum = " [enum]",
+  Field = "ﰠ [field]",
+  Keyword = " [key]",
+  Variable = " [variable]",
+  Folder = " [folder]",
+  Snippet = " [snippet]",
+  Operator = " [operator]",
+  Module = " [module]",
+  Text = "ﮜ[text]",
+  Class = " [class]",
+  Interface = " [interface]"
+}
+
