4ab5574859556a779d07bc4941891b26e403f3b1
Bastien Rivière <babathriviere@gmail.com>
2021-02-09 14:39:12 +0100

    chore(nvim): use luafmt instead of lua-formatter

--- config/nvim/lua/baba/formatter.lua
+++ config/nvim/lua/baba/formatter.lua
@@ -1,23 +1,30 @@
-require("formatter").setup({
-  filetype = {
-    css = {
-      function()
-        return {
-          exe = "prettier",
-          args = {"--stdin-filepath", vim.api.nvim_buf_get_name(0), "--single-quote"},
-          stdin = true
-        }
-      end
-    },
-    elixir = {
-      function()
-        return {exe = "mix", args = {"format", "-"}, stdin = true}
-      end
-    },
-    lua = {
-      function()
-        return {exe = "lua-format", args = {"--config=$HOME/.config/luaformatter/config.yaml"}, stdin = true}
-      end
+require("formatter").setup(
+  {
+    filetype = {
+      css = {
+        function()
+          return {
+            exe = "prettier",
+            args = {"--stdin-filepath", vim.api.nvim_buf_get_name(0), "--single-quote"},
+            stdin = true
+          }
+        end
+      },
+      elixir = {
+        function()
+          return {exe = "mix", args = {"format", "-"}, stdin = true}
+        end
+      },
+      lua = {
+        function()
+          -- return {exe = "lua-format", args = {"--config=$HOME/.config/luaformatter/config.yaml"}, stdin = true}
+          return {
+            exe = "luafmt",
+            args = {"--indent-count", 2, "--stdin"},
+            stdin = true
+          }
+        end
+      }
     }
   }
-})
+)
