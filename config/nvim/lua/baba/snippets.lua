b9d6f890913c4e48dd38b7f0f4c245b4e245622c
Bastien Rivière <babathriviere@gmail.com>
2021-02-17 11:36:12 +0100

    chore(nvim): add snippets

--- config/nvim/lua/baba/snippets.lua
+++ config/nvim/lua/baba/snippets.lua
@@ -25,80 +25,95 @@ snips._global = {
 }
 
 snips.elixir = {
+  --
+  -- Functions
+  --
   def = utils.match_indentation [[
 def $1($2) do
   $0
 end]],
-
   defp = utils.match_indentation [[
 defp $1($2) do
   $0
-end
-  ]],
-
+end]],
   ["def:"] = "def ${1:myfunc}($2), do: $0",
   ["defp:"] = "defp ${1:myfunc}($2), do: $0",
-
+  --
+  -- Module
+  --
   -- TODO(babariviere): try to determine module from path
   defm = utils.match_indentation [[
 defmodule $1 do
   $0
 end]],
-
   deft = utils.match_indentation [[
 defmodule $1 do
   use ${2:$1}.${3:DataCase}
 end
 ]],
-
   ["test"] = utils.match_indentation [[
 test "$1" do
   $0
 end]],
-
-  iex = utils.comment_and_indent [[
-    iex> $0]],
-
+  --
+  -- Blocks
+  --
   ["do"] = utils.match_indentation [[
 do
   $0
 end]],
-
   ["do:"] = "do: $0",
-
+  --
+  -- Lambda
+  --
   ["fn"] = "fn ${1:args} -> $0 end",
-
+  --
+  -- Conditions
+  --
   ["case"] = utils.match_indentation [[
 case $1 do
   $2 ->
     $0
 end]],
-
   ["if"] = utils.match_indentation [[
 if $1 do
   $0
 end]],
   ["if:"] = utils.match_indentation "if $1, do: $0",
   ["ife:"] = utils.match_indentation "if $1, do: $2, else: $0",
-
+  --
+  -- Enum
+  --
+  ["map"] = "Enum.map($1, fn $2 -> $0 end)",
+  ["each"] = "Enum.each($1, fn $2 -> $0 end)",
+  --
+  -- Pipe
+  --
   ["p"] = "|> $0",
   [">e"] = "|> Enum.each(fn $1 -> $0 end)",
-  [">i"] = "|> IO.inspect()$0",
-
-  ["put"] = "IO.puts($1)$0",
-  ["ins"] = "IO.inspect($1)$0",
-
+  [">i"] = "|> IO.inspect()",
+  [">m"] = "|> Enum.map(fn $1 -> $0 end)",
+  --
+  -- IO
+  --
+  ["put"] = "IO.puts($1)",
+  ["ins"] = "IO.inspect($1)",
+  --
+  -- Documentation
+  --
   ["doc"] = utils.match_indentation [[
 @doc """
 $0
 """
-]]
+]],
+  iex = utils.comment_and_indent [[
+    iex> $0]]
 }
 
 snips.go = {
   err = utils.match_indentation [[
 if err != nil {
-  return$0
+  return ${1:err}
 }]]
 }
 
