e8345692813b831356a00c8a75504829dc87293a
Bastien Rivière <babathriviere@gmail.com>
2021-04-03 19:30:35 +0200

    fix(nvim): barbar color with dracula

--- config/nvim/lua/baba/themes/dracula.lua
+++ config/nvim/lua/baba/themes/dracula.lua
@@ -6,3 +6,4 @@ vim.g.airline_theme = "dracula"
 vim.api.nvim_command [[colorscheme dracula]]
 
 -- vim.api.nvim_command [[highlight DraculaDiffDelete guibg=none ctermbg=none]]
+vim.api.nvim_command [[highlight BufferCurrentMod guifg=#f8f8f2 guibg=#282a36]]
