8fe99b11395373dee7034455b527b2a212437d79
Bastien Riviere <babathriviere@gmail.com>
2021-06-23 17:42:54 +0200

    chore(emacs): add wrapper around emacsclient

--- modules/editor/emacs.nix
+++ modules/editor/emacs.nix
@@ -3,6 +3,15 @@
 let
   cfg = config.my.editor.emacs;
   emacs = pkgs.emacsOsx;
+  emacsclient = pkgs.writeScriptBin "emacsclient" ''
+    #!${pkgs.runtimeShell}
+
+    if [ -z "$1" ]; then
+        ${emacs}/bin/emacsclient -c -n -a "" "$@"
+    else
+        ${emacs}/bin/emacsclient -n -a "" "$@"
+    fi
+  '';
 in {
   options.my.editor.emacs = with lib; { enable = mkEnableOption "emacs"; };
 
@@ -11,6 +20,10 @@ in {
       programs.emacs = {
         enable = true;
         package = emacs;
+      };
+
+      programs.zsh = {
+        shellAliases = { e = "${emacsclient}/bin/emacsclient"; };
       };
     };
 
@@ -33,6 +46,11 @@ in {
     services.emacs = {
       enable = true;
       package = emacs;
+    };
+
+    env = {
+      EDITOR = "${emacsclient}/bin/emacsclient";
+      VISUAL = "${emacsclient}/bin/emacsclient";
     };
   };
 }
