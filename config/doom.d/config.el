9e96a3be25869245c4caed876a83cdddc6ade414
Bastien Riviere <babathriviere@gmail.com>
2021-07-07 14:48:55 +0200

    style(emacs): use MonoLisa + dracula

--- config/doom.d/config.el
+++ config/doom.d/config.el
@@ -21,13 +21,13 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "Fira Code" :size 12))
+(setq doom-font (font-spec :family "MonoLisa" :size 12))
 ;;doom-variable-pitch-font (font-spec :family "sans" :size 13))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-flatwhite)
+(setq doom-theme 'doom-dracula)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
