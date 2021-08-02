f143bdeda65c5a76945ad323a6ca86e429ccde84
Bastien Riviere <babathriviere@gmail.com>
2021-08-02 15:25:26 +0200

    chore(cl): add config files

--- home/profiles/dev/common-lisp.nix
+++ home/profiles/dev/common-lisp.nix
@@ -2,4 +2,10 @@
 
 {
   home.packages = [ pkgs.sbcl ];
+
+  home.file.".sbclrc".source = config.dotfiles.configDir + "/sbclrc";
+
+  xdg.configFile."common-lisp/source-registry.conf.d/50-projects.conf".text = ''
+    (:tree (:home "src/github.com/babariviere"))
+  '';
 }
