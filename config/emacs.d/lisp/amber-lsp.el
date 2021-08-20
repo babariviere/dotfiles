e9bcb922726044809bd73e8b6a4c1182a2d7b76d
Bastien Riviere <babathriviere@gmail.com>
2021-08-20 13:22:05 +0200

    chore(emacs): configure lsp

--- config/emacs.d/lisp/amber-lsp.el
+++ config/emacs.d/lisp/amber-lsp.el
@@ -1,7 +1,13 @@
 (require 'use-package)
 
+;; TODO: look at the bug fixes in this file
+;; https://github.com/hlissner/doom-emacs/blob/develop/modules/tools/lsp/%2Blsp.el
 (use-package lsp-mode
   :hook ((lsp-mode . lsp-enable-which-key-integration))
+  :custom
+  (lsp-enable-folding nil)
+  (lsp-enable-on-type-formatting nil)
+  (lsp-headerline-breadcrumb-enable nil)
   :general
   ('motion
    lsp-mode-map
