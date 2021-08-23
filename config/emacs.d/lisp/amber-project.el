d7e223b133fb9649f37759c0ed05a4c64b3bb3d8
Bastien Riviere <babathriviere@gmail.com>
2021-08-23 21:07:11 +0200

    chore(emacs): setup projectile search path

--- config/emacs.d/lisp/amber-project.el
+++ config/emacs.d/lisp/amber-project.el
@@ -7,10 +7,15 @@
   (projectile-completion-system 'auto)
   (projectile-switch-project-action #'projectile-dired)
   (projectile-globally-ignored-files '(".DS_Store" "TAGS"))
+  (projectile-project-search-path '("~/src" "~/src/github.com/babariviere"))
+  (projectile-project-root-files-functions '(projectile-root-local
+					     projectile-root-top-down
+					     projectile-root-bottom-up
+					     projectile-root-top-down-recurring))
   :general
   (amber/leader-keys
-   "SPC" '(projectile-find-file :which-key "find file in project")
-   "p" '(:ignore t :which-key "project")
-   "pp" '(projectile-switch-project :wk "switch project")))
+    "SPC" '(projectile-find-file :which-key "find file in project")
+    "p" '(:ignore t :which-key "project")
+    "pp" '(projectile-switch-project :wk "switch project")))
 
 (provide 'amber-project)
