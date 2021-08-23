0914bfbd258b4b60282cf4f087e402ab6d881b8d
Bastien Rivière <bastienriviere@ochatt.babariviere.github.beta.tailscale.net>
2021-08-23 10:52:57 +0200

    chore(emacs): start git-commit-mode in insert state

--- config/emacs.d/lisp/amber-magit.el
+++ config/emacs.d/lisp/amber-magit.el
@@ -1,10 +1,16 @@
 (require 'use-package)
 
+(defun amber/git-commit-in-insert-state-h ()
+  "Start git-commit-mode in insert state only if buffer is blank."
+  (when (and (bound-and-true-p evil-mode) (bobp) (eolp))
+    (evil-insert-state)))
+
 (use-package magit
+  :hook (git-commit-setup . amber/git-commit-in-insert-state-h)
   :general
   (amber/leader-keys
-   "g" '(:ignore t :wk "git")
-   "gg" '(magit-status :wk "git status")))
+    "g" '(:ignore t :wk "git")
+    "gg" '(magit-status :wk "git status")))
 
 (use-package evil-collection-magit
   :after magit)
