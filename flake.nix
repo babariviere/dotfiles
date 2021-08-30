8bfc35d9a5ced1d05b9d8d41189135594289b0b3
Bastien Riviere <babathriviere@gmail.com>
2021-08-30 08:37:50 +0200

    chore(flake): select supported systems

--- flake.nix
+++ flake.nix
@@ -181,7 +181,7 @@
         python = pythonOverlay;
         self = self.overlay;
       };
-    } // (utils.lib.eachDefaultSystem (system:
+    } // (utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
       let
         pkgs = import nixpkgs {
           inherit system;
