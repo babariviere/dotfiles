b6f82876b8f28a05d1ad4fd1cf39550d52fdebd5
Bastien Riviere <babathriviere@gmail.com>
2021-08-31 11:22:15 +0200

    chore(emacs): emacs-osx -> emacsGit

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,8 +2,8 @@ final: prev:
 
 {
   cat-with-cat = prev.callPackage ./cat-with-cat { };
-  emacsOsx = prev.callPackage ./emacs { };
-  amber-emacs = prev.callPackage ./amber-emacs { emacs = final.emacsOsx; };
+  emacsGit = prev.callPackage ./emacs { emacsGit = prev.emacsGit; };
+  amber-emacs = prev.callPackage ./amber-emacs { emacs = final.emacsGit; };
   lima = prev.callPackage ./lima { };
   tailscale = prev.callPackage ./tailscale { inherit (prev) tailscale; };
 } // (prev.lib.optionalAttrs (prev.system == "x86_64-darwin") {
--- pkgs/emacs/default.nix
+++ pkgs/emacs/default.nix
@@ -4,8 +4,6 @@ builtins.foldl' (drv: fn: fn drv) emacsGit [
   (drv: drv.override ({ nativeComp = true; }))
   (drv:
     drv.overrideAttrs (attrs: {
-      name = "emacs-osx-${attrs.version}";
-
       CFLAGS = "-DMAC_OS_X_VERSION_MAX_ALLOWED=110200 -g -O2";
     }))
 ]
