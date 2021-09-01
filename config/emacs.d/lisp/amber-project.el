cdf99223faa4e796ad74ee0b2940883f9eb73858
Bastien Riviere <babathriviere@gmail.com>
2021-09-01 14:28:11 +0200

    chore(emacs): projectile search path

--- config/emacs.d/lisp/amber-project.el
+++ config/emacs.d/lisp/amber-project.el
@@ -1,5 +1,10 @@
 (require 'use-package)
 
+(defun amber/project-ignored-p (project-root)
+  "Return non-nil if PROJECT-ROOT must be ignored from project list."
+  (let ((filename (f-filename project-root)))
+    (equal filename ".git")))
+
 (use-package projectile
   :after general
   :config
@@ -9,11 +14,14 @@
   (projectile-completion-system 'auto)
   (projectile-switch-project-action #'projectile-dired)
   (projectile-globally-ignored-files '(".DS_Store" "TAGS"))
-  (projectile-project-search-path '("~/src" "~/src/github.com/babariviere"))
+  ;; Run `projectile-discover-projects-in-search-path` manually instead.
+  (projectile-auto-discover nil)
+  (projectile-project-search-path '(("~/src" . 3)))
   (projectile-project-root-files-functions '(projectile-root-local
 					     projectile-root-top-down
 					     projectile-root-bottom-up
 					     projectile-root-top-down-recurring))
+  (projectile-ignored-project-function #'amber/project-ignored-p)
   :general
   (amber/leader-keys
     "SPC" '(projectile-find-file :which-key "find file in project")
