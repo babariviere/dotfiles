8735bb18798aa33f087ef6ae2588bb42e94ae4ca
Bastien Riviere <babathriviere@gmail.com>
2021-09-02 09:53:17 +0200

    chore(ochatt): add script to copy applications

--- hosts/ochatt.nix
+++ hosts/ochatt.nix
@@ -127,7 +127,11 @@ in {
     fonts = [ pkgs.source-sans-pro ];
   };
 
-  home-manager.users.bastienriviere = {
+  system.activationScripts.applications.text = lib.mkForce ''
+    true
+  '';
+
+  home-manager.users.bastienriviere = { config, lib, pkgs, ... }: {
     profiles = {
       dev = {
         common-lisp.enable = true;
@@ -195,6 +199,28 @@ in {
     programs.fzf.enable = true;
     programs.nix-index.enable = true;
     programs.ssh.enable = true;
+
+    home.activation = {
+      copyApplications = let
+        apps = pkgs.buildEnv {
+          name = "home-manager-applications";
+          paths = config.home.packages;
+          pathsToLink = "/Applications";
+        };
+      in lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+        echo "setting up ~/Applications..." >&2
+        baseDir="$HOME/Applications/Home Manager Apps"
+        if [ -d "$baseDir" ]; then
+          rm -rf "$baseDir"
+        fi
+        mkdir -p "$baseDir"
+        for appFile in ${apps}/Applications/*; do
+          target="$baseDir/$(basename "$appFile")"
+          $DRY_RUN_CMD cp ''${VERBOSE_ARG:+-v} -fHRL "$appFile" "$baseDir"
+          $DRY_RUN_CMD chmod ''${VERBOSE_ARG:+-v} -R +w "$target"
+        done
+      '';
+    };
 
     home.stateVersion = "21.03";
   };
