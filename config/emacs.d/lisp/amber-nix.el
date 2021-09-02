c41fdd600ab676f7e05cb42c47d658320e58bab2
Bastien Riviere <babathriviere@gmail.com>
2021-09-02 09:52:35 +0200

    chore(emacs): fix default formatter for nix

--- config/emacs.d/lisp/amber-nix.el
+++ config/emacs.d/lisp/amber-nix.el
@@ -1,7 +1,12 @@
 (require 'use-package)
 
+(defun amber/nix-set-formatter ()
+  "Set default nix formatter for format-all."
+  (setq-local format-all-formatters '(("Nix" nixfmt))))
+
 (use-package nix-mode
   :mode "\\.nix\\'"
+  :hook (format-all-mode . amber/nix-set-formatter)
   :init
   (add-to-list 'auto-mode-alist
                (cons "/flake\\.lock\\'"
@@ -9,9 +14,9 @@
   (amber/set-company-backend 'nix-mode 'company-nixos-options)
   :general
   (amber/local-leader-keys nix-mode-map
-   "f" '(nix-format-buffer :wk "format buffer")
-   "r" '(nix-repl-show :wk "repl")
-   "s" '(nix-shell :wk "nix-shell")))
+    "f" '(nix-format-buffer :wk "format buffer")
+    "r" '(nix-repl-show :wk "repl")
+    "s" '(nix-shell :wk "nix-shell")))
 
 (use-package nix-drv-mode
   :mode "\\.drv\\'")
