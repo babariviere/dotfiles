4b724093a320c365e6616921348803f3366d089b
Bastien Riviere <babathriviere@gmail.com>
2021-09-02 16:29:36 +0200

    feat: add EmacsClient.app

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,6 +3,10 @@ final: prev:
 {
   cat-with-cat = prev.callPackage ./cat-with-cat { };
   emacsGit = prev.callPackage ./emacs { emacsGit = prev.emacsGit; };
+  emacs-client = prev.runCommand "emacs-client" { } ''
+    mkdir -p $out/Applications
+    cp -r ${../resources/EmacsClient.app} $out/Applications/EmacsClient.app
+  '';
   amber-emacs = prev.callPackage ./amber-emacs { emacs = final.emacsGit; };
   lima = prev.callPackage ./lima { };
   tailscale = prev.callPackage ./tailscale { inherit (prev) tailscale; };
