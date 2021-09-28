0033a04d9a37c2fbdd3e1869f808778b4503c368
Bastien Riviere <babathriviere@gmail.com>
2021-09-28 17:09:40 +0200

    chore(nix): change binary cache order

--- flake.nix
+++ flake.nix
@@ -35,18 +35,16 @@
 
         nix = {
           binaryCaches = [
-            "http://nix-store.vercar.home"
-            "https://cache.nixos.org"
             "https://cache.ngi0.nixos.org/"
             "https://nix-community.cachix.org"
             "https://babariviere.cachix.org"
+            "http://nix-store.vercar.home"
           ];
           binaryCachePublicKeys = [
-            "vercar.home:1R/Q2IoFGQ5NqYGneOoxhrvkhgpdBrn87Po2uPM4wmI="
-            "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
             "cache.ngi0.nixos.org-1:KqH5CBLNSyX184S9BKZJo1LxrxJ9ltnY2uAs5c/f1MA="
             "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
             "babariviere.cachix.org-1:igoOZJyEAhWg3Rbavjim3yyDj7nIkGYe5327+G0diFw="
+            "vercar.home:1R/Q2IoFGQ5NqYGneOoxhrvkhgpdBrn87Po2uPM4wmI="
           ];
           extraOptions = ''
             experimental-features = nix-command flakes
