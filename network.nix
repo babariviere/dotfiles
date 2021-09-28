5eb26ed1fb4279f2f1a626afa0f5685dd2e10f16
Bastien Riviere <babathriviere@gmail.com>
2021-09-28 16:51:06 +0200

    feat: add beehum to network

--- network.nix
+++ network.nix
@@ -13,5 +13,11 @@
       public-key =
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMO6/jzKplTgI8UEbJTdNskExUfQEJ9GG1b9smYXoaP4";
     }
+    {
+      name = "beehum";
+      ip = "100.89.219.104";
+      public-key =
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAR8+m6kSrZ8Zc3+NMolR2mzmfgzFPhij/cGl2KGHDBQ";
+    }
   ];
 }
