58a630854ce15139ff3cf5f3185ca6af214c4142
Bastien Riviere <babathriviere@gmail.com>
2021-09-28 16:45:55 +0200

    chore(emacs): add all-the-icons font

--- home/profiles/editor/emacs.nix
+++ home/profiles/editor/emacs.nix
@@ -14,7 +14,7 @@ in lib.mkMerge [
         recursive = true;
       };
     };
-    home.packages = [ emacs ]
+    home.packages = [ emacs pkgs.emacs-all-the-icons-fonts ]
       ++ (lib.optionals pkgs.stdenv.isDarwin [ pkgs.emacs-client ]);
     programs.emacs.package = emacs;
     shell.aliases = { e = "${emacs}/bin/emacsclient"; };
@@ -23,6 +23,9 @@ in lib.mkMerge [
       EDITOR = "${emacs}/bin/emacsclient -nw";
       VISUAL = "${emacs}/bin/emacsclient";
     };
+
+    # Ensure all-the-icons-fonts will be installed
+    fonts.fontconfig.enable = true;
   }
   (lib.mkIf pkgs.stdenv.isLinux {
     services.emacs = {
