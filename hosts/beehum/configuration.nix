e3f29a6eb78add43fb48525ded4ef4313d45f4c2
Bastien Riviere <babathriviere@gmail.com>
2021-09-30 16:28:22 +0200

    fix(mako): make text readable

--- hosts/beehum/configuration.nix
+++ hosts/beehum/configuration.nix
@@ -233,7 +233,7 @@
       defaultTimeout = 0;
       backgroundColor = "#282a36";
       borderColor = "#282a36";
-      textColor = "#44475a";
+      textColor = "#f8f8f2";
       extraConfig = ''
         [urgency=low]
         border-color=#282a36
