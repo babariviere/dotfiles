307551a44b0b46ab49faa55acd75e5ec10420284
Bastien Riviere <babathriviere@gmail.com>
2021-09-30 16:11:33 +0200

    feat(mako): add mako settings

--- hosts/beehum/configuration.nix
+++ hosts/beehum/configuration.nix
@@ -227,6 +227,25 @@
 
     home.sessionPath = [ "~/.local/bin" ];
 
+    programs.mako = {
+      enable = true;
+      font = "MonoLisa 10";
+      defaultTimeout = 2000;
+      backgroundColor = "#282a36";
+      borderColor = "#282a36";
+      textColor = "#44475a";
+      extraConfig = ''
+        [urgency=low]
+        border-color=#282a36
+
+        [urgency=normal]
+        border-color=#f1fa8c
+
+        [urgency=high]
+        border-color=#ff5555
+      '';
+    };
+
     programs.rofi = {
       enable = true;
       theme = let inherit (config.lib.formats.rasi) mkLiteral;
@@ -617,7 +636,6 @@
             command =
               "${pkgs.systemd}/bin/systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP";
           }
-
         ];
       };
       extraConfig = ''
