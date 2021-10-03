602135c7ac07f6245377f909dbac6fcaa00ce734
Bastien Riviere <babathriviere@gmail.com>
2021-10-03 17:38:50 +0200

    chore(go): remove gotools

--- home/profiles/dev/go.nix
+++ home/profiles/dev/go.nix
@@ -1,7 +1,7 @@
 { config, lib, pkgs, ... }:
 
 {
-  home.packages = with pkgs; [ go gopls gotools ];
+  home.packages = with pkgs; [ go gopls ];
 
   shell.env.GOPATH = "${config.home.homeDirectory}/src";
 }
