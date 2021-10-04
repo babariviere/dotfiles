890ff4e4d010a54a2d02e312f48ab0ef2d749dcc
Bastien Riviere <babathriviere@gmail.com>
2021-10-04 17:15:25 +0200

    chore(emacs): do not automatically projects

--- config/emacs.d/lisp/amber-project.el
+++ config/emacs.d/lisp/amber-project.el
@@ -16,11 +16,12 @@
   (projectile-globally-ignored-files '(".DS_Store" "TAGS"))
   ;; Run `projectile-discover-projects-in-search-path` manually instead.
   (projectile-auto-discover nil)
+  (projectile-track-known-projects-automatically nil)
   (projectile-project-search-path '(("~/src" . 3)))
   (projectile-project-root-files-functions '(projectile-root-local
-					     projectile-root-top-down
-					     projectile-root-bottom-up
-					     projectile-root-top-down-recurring))
+											 projectile-root-top-down
+											 projectile-root-bottom-up
+											 projectile-root-top-down-recurring))
   (projectile-ignored-project-function #'amber/project-ignored-p)
   :general
   (amber/leader-keys
