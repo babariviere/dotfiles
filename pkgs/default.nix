8ef85290406593ec03f59125385bfaa60e3c6e49
Bastien Riviere <babathriviere@gmail.com>
2021-10-06 18:04:48 +0200

    chore(pkgs): remove emacsGit

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,15 +2,13 @@ final: prev:
 
 {
   cat-with-cat = prev.callPackage ./cat-with-cat { };
-  amber-emacs =
-    prev.callPackage ./amber-emacs { emacs = final.emacsGit or prev.emacsGit; };
+  amber-emacs = prev.callPackage ./amber-emacs { emacs = prev.emacsPgtkGcc; };
   lima = prev.callPackage ./lima { };
   # FIXME: unable to patch
   # nixUnstable = prev.callPackage ./nix { nix = prev.nixUnstable; };
   tailscale = prev.callPackage ./tailscale { inherit (prev) tailscale; };
   gitlab-ci-lint = prev.callPackage ./gitlab-ci-lint { };
 } // (prev.lib.optionalAttrs (prev.system == "x86_64-darwin") {
-  emacsGit = prev.callPackage ./emacs { emacsGit = prev.emacsGit; };
   emacs-client = prev.callPackage ./emacs-client { emacs = final.emacsGit; };
   lunchy = prev.callPackage ./lunchy { };
 })
--- pkgs/emacs/default.nix
+++
@@ -1,9 +0,0 @@
-{ emacsGit, ... }:
-
-builtins.foldl' (drv: fn: fn drv) emacsGit [
-  (drv: drv.override ({ nativeComp = true; }))
-  (drv:
-    drv.overrideAttrs (attrs: {
-      CFLAGS = "-DMAC_OS_X_VERSION_MAX_ALLOWED=110200 -g -O2";
-    }))
-]
