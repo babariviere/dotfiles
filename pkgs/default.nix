e4e0c5228c1489180e5b3c51a4b1380c5fee82c9
Bastien Riviere <babathriviere@gmail.com>
2021-10-06 09:36:11 +0200

    feat(nix): patch nix to allow untracked flake.nix
    Allow nix to use untracked flake.nix.
    This hack is ugly, and should not be used.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -5,6 +5,7 @@ final: prev:
   amber-emacs =
     prev.callPackage ./amber-emacs { emacs = final.emacsGit or prev.emacsGit; };
   lima = prev.callPackage ./lima { };
+  nixUnstable = prev.callPackage ./nix { nix = prev.nixUnstable; };
   tailscale = prev.callPackage ./tailscale { inherit (prev) tailscale; };
   gitlab-ci-lint = prev.callPackage ./gitlab-ci-lint { };
 } // (prev.lib.optionalAttrs (prev.system == "x86_64-darwin") {
