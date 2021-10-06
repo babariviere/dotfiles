a99c2123c16d2384bc3e4b1cb0af2765fa566f47
Bastien Riviere <babathriviere@gmail.com>
2021-10-06 13:27:44 +0200

    chore(emacs): add gitlab lsp

--- config/emacs.d/lisp/amber-data.el
+++ config/emacs.d/lisp/amber-data.el
@@ -32,11 +32,21 @@
 		 ("\\.yaml\\'" . yaml-mode))
   :hook (yaml-mode . lsp))
 
+(defun amber/gitlab-inject-api-token (fn &rest args)
+  "Inject gitlab-ci-api-token using authinfo."
+  (let* ((auth-source (car (auth-source-search :host "gitlab.com/api/v4" :requires '(secret))))
+		 (gitlab-ci-api-token (funcall (plist-get auth-source :secret))))
+	(apply fn args)))
+
 (use-package gitlab-ci-mode
   :mode (("\\.gitlab-ci.yml\\'" . gitlab-ci-mode))
   :hook (gitlab-ci-mode . lsp)
   :config
-  (push 'gitlab-ci-mode (lsp--client-major-modes (gethash 'yamlls lsp-clients))))
+  (add-to-list 'lsp-language-id-configuration '(gitlab-ci-mode . "spring-boot-properties-yaml"))
+  (add-to-list 'lsp-yaml-custom-tags "!reference sequence")
+  (push 'gitlab-ci-mode (lsp--client-major-modes (gethash 'yamlls lsp-clients)))
+
+  (advice-add 'gitlab-ci-lint :around #'amber/gitlab-inject-api-token))
 
 (provide 'amber-data)
 
