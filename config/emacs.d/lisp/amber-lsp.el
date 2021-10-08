b2e2e31f71cef765a8acd2748631f9e91659c0f2
Bastien Riviere <babathriviere@gmail.com>
2021-10-08 14:46:10 +0200

    chore: add header for lsp

--- config/emacs.d/lisp/amber-lsp.el
+++ config/emacs.d/lisp/amber-lsp.el
@@ -1,7 +1,37 @@
+;;; amber-lsp.el --- summary -*- lexical-binding: t -*-
+
+;; Author:
+;; Maintainer:
+;; Version: version
+;; Package-Requires: (dependencies)
+;; Homepage: homepage
+;; Keywords: keywords
+
+
+;; This file is not part of GNU Emacs
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; LSP support to Amber Emacs.
+
+;;; Code:
+
 (require 'use-package)
 
 ;; TODO: look at the bug fixes in this file
 ;; https://github.com/hlissner/doom-emacs/blob/develop/modules/tools/lsp/%2Blsp.el
+
 (use-package lsp-mode
   :demand t
   :hook ((lsp-mode . lsp-enable-which-key-integration))
@@ -14,7 +44,7 @@
   ;; breaks sideline
   (lsp-eldoc-enable-hover nil)
   :general
-  ('normal lsp-mode-map
+  ('motion lsp-mode-map
 		   "gd" #'lsp-find-type-definition
 		   "K" #'lsp-describe-thing-at-point))
 
@@ -27,3 +57,5 @@
    [remap xref-find-apropos] #'consult-lsp-symbols))
 
 (provide 'amber-lsp)
+
+;;; amber-lsp.el ends here
