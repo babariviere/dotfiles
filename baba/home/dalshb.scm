8da610d32d5beb0dc450730cf517a97f1e396d1f
Bastien Riviere <babathriviere@gmail.com>
2021-10-21 21:13:44 +0200

    feat: add configuration and channels

---
+++ baba/home/dalshb.scm
@@ -0,0 +1,201 @@
+(define-module (baba home dalshb))
+(use-modules (gnu home)
+	     (gnu home services)
+	     (gnu home services shells)
+	     (gnu home-services emacs)
+	     (gnu home-services gnupg)
+	     (gnu home-services version-control)
+	     (gnu home-services wm)
+	     (gnu packages admin)
+	     (gnu packages cmake)
+	     (gnu packages emacs)
+	     (gnu packages emacs-xyz)
+	     (gnu packages wm)
+	     (gnu services)
+	     (guix gexp)
+	     (guix packages)
+	     (baba packages emacs-xyz)
+	     (flat packages emacs))
+
+;; (define emacs-dash-patched
+;;   (package
+;;     (inherit emacs-dash)
+;;     (arguments
+;;       `(#:phases
+;; 	(modify-phases %standard-phases
+;; 	  (add-after 'unpack 'modify-makefile
+;; 	    (lambda* (#:key outputs #:allow-other-keys)
+;; 		     (substitute* "Makefile"
+;; 			(("byte-compile-error-on-warn t")
+;; 			 "byte-compile-error-on-warn nil"))
+;; 		     #t)))))))
+;; 
+;; (define replace-broken-packages
+;;   (package-input-rewriting `((,emacs-dash . ,emacs-dash-patched))) #:deep? #f)
+
+(define %elisp-packages
+  (list
+   emacs-doom-modeline
+   emacs-helpful
+   emacs-use-package
+   emacs-which-key
+   emacs-gcmh
+   emacs-hl-todo
+   emacs-git-gutter
+   emacs-git-gutter-fringe
+   emacs-exec-path-from-shell
+   emacs-vertico
+   emacs-marginalia
+   emacs-orderless
+   emacs-consult
+   emacs-embark
+   ;; emacs-embark-consult
+   
+   emacs-projectile
+   
+   emacs-evil
+   emacs-evil-collection
+   emacs-evil-surround
+   emacs-evil-commentary
+   emacs-general
+   emacs-undo-fu
+   emacs-undo-fu-session
+   emacs-eldoc
+   
+   emacs-magit
+   emacs-magit-todos
+   emacs-forge
+   
+   emacs-diredfl
+   emacs-all-the-icons-dired
+   emacs-diff-hl
+   
+   emacs-sly
+   emacs-sly-macrostep
+   emacs-sly-repl-ansi-color
+   
+   emacs-lispy
+   emacs-lispyville
+   
+   emacs-ccls
+   emacs-cmake-mode
+   emacs-demangle-mode
+   emacs-disaster
+   emacs-modern-cpp-font-lock
+   
+   emacs-company
+   emacs-company-box
+   
+   emacs-csharp-mode
+   
+   emacs-yaml-mode
+   emacs-gitlab-ci-mode
+   
+   emacs-envrc
+   
+   emacs-eros
+   emacs-highlight-quoted
+   emacs-elisp-demos
+   emacs-macrostep
+   
+   emacs-elixir-mode
+   emacs-alchemist
+   emacs-exunit
+   
+   emacs-fish-mode
+   
+   emacs-flycheck
+   emacs-flycheck-inline
+   
+   ;; emacs-format-all
+   
+   emacs-go-mode
+   emacs-go-guru
+   
+   emacs-lsp-mode
+   emacs-lsp-ui
+   emacs-consult-lsp
+   
+   emacs-org
+   emacs-org-appear
+   emacs-org-contrib
+   emacs-org-edna
+   emacs-org-superstar
+   emacs-evil-org
+   emacs-org-roam
+   
+   emacs-nix-mode
+   emacs-nix-update
+   
+   ;; emacs-perspective
+   ;; emacs-persp-projectile
+   
+   ;; emacs-python-mode
+   emacs-poetry
+   emacs-lsp-python-ms
+   
+   emacs-smartparens
+   
+   emacs-yasnippet
+   emacs-doom-snippets
+   
+   ;; emacs-doom-themes
+   emacs-kaolin-themes
+   
+   emacs-vterm
+   
+   emacs-editorconfig
+   emacs-git-auto-commit-mode
+   emacs-zoom
+   ;; emacs-pinentry
+   ))
+
+(define %sway-config
+  `((include ,(file-append sway "/etc/sway/config"))
+    (bindsym $mod+Shift+e exec emacsclient -c --eval "(eshell)")))
+
+(home-environment
+ (packages
+  (list htop))
+ (services
+  (list (service home-emacs-service-type
+		 (home-emacs-configuration
+		  (package emacs-pgtk-native-comp)
+		  (elisp-packages %elisp-packages)
+		  (server-mode? #t)
+		  (rebuild-elisp-packages? #f)
+		  (xdg-flavor? #t)))
+	(service home-bash-service-type
+		 (home-bash-configuration
+		  (guix-defaults? #t)))
+	(service home-fish-service-type
+		 (home-fish-configuration
+		  (abbreviations '(("gco" . "git checkout")
+				   ("gs" . "git status")
+				   ("gsr" . "sudo -E guix system reconfigure")
+				   ("ghr" . "guix home reconfigure")))))
+	(service home-gnupg-service-type
+		 (home-gnupg-configuration
+		   (gpg-agent-config
+		     (home-gpg-agent-configuration
+		       ;; (pinentry-flavor 'emacs)
+		       (extra-config
+			 '((max-cache-ttl . 86400)))
+		       ))))
+	(service home-git-service-type
+		 (home-git-configuration
+		   (config
+		     `((user
+			 ((name . "Bastien Riviere")
+			  (email . "babathriviere@gmail.com")
+			  (signingKey . "F9B7864F2AB46F18")))
+		       (github
+			 ((user . "babariviere")))
+		       (remote
+			 ((pushDefault . "origin")))
+		       (commit
+			 ((gpgSign . #t)))))))
+	(service home-sway-service-type
+		 (home-sway-configuration
+		  (config %sway-config))))))
+
---
+++ etc/channels.scm
@@ -0,0 +1,29 @@
+(cons* (channel
+	(name 'nonguix)
+        (url "https://gitlab.com/nonguix/nonguix")
+        ;; Enable signature verification:
+        (introduction
+         (make-channel-introduction
+          "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+          (openpgp-fingerprint
+           "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+       (channel
+        (name 'flat)
+        (url "https://github.com/flatwhatson/guix-channel.git")
+        (introduction
+         (make-channel-introduction
+          "33f86a4b48205c0dc19d7c036c85393f0766f806"
+          (openpgp-fingerprint
+           "736A C00E 1254 378B A982  7AF6 9DBE 8265 81B6 4490"))))
+       (channel
+	 (name 'baba)
+	 (url "file:///home/babariviere/guix"))
+       (channel
+	 (name 'rde)
+	 (url "https://git.sr.ht/~abcdw/rde")
+	 (introduction
+	  (make-channel-introduction
+	   "257cebd587b66e4d865b3537a9a88cccd7107c95"
+	   (openpgp-fingerprint
+	    "2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0"))))
+ %default-channels)
