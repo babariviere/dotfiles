092f8f6ce6df2e4241b13d1d5eedb8d9ccb43721
Bastien Riviere <babathriviere@gmail.com>
2021-10-31 18:16:29 +0100

    fix: canonicalize-path for channel-root

--- baba.scm
+++ baba.scm
@@ -3,9 +3,10 @@
   #:export (%channel-root))
 
 (define %channel-root
-  (find (lambda (path)
-	  (and
-	   (file-exists? (string-append path "/.guix-channel"))
-	   (file-exists? (string-append path "/baba"))
-	   (file-exists? (string-append path "/baba.scm"))))
-	%load-path))
+  (canonicalize-path
+   (find (lambda (path)
+	   (and
+	    (file-exists? (string-append path "/.guix-channel"))
+	    (file-exists? (string-append path "/baba"))
+	    (file-exists? (string-append path "/baba.scm"))))
+	 %load-path)))
