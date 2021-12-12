76fcf254ff5a04b2cf2704cb0a208a16640cd018
Bastien Riviere <babathriviere@gmail.com>
2021-12-12 19:49:13 +0100

    feat(emacs): fully use guix-emacs channel

--- baba/home/services/emacs.scm
+++ baba/home/services/emacs.scm
@@ -2,6 +2,7 @@
   #:use-module (baba)
   #:use-module (gnu packages emacs-xyz)
   #:use-module ((gnu packages emacs-xyz) #:prefix e:)
+  #:use-module (emacs build-system melpa)
   #:use-module (emacs packages melpa)
   #:use-module (flat packages emacs)
   #:use-module (gnu home services)
@@ -12,150 +13,172 @@
   #:use-module (gnu services)
   #:use-module (guix gexp)
   #:use-module (guix packages)
+  #:use-module (guix utils)
   #:use-module (ice-9 ftw)
   #:use-module (ice-9 match)
   #:export (emacs-service))
 
 (define rewrite-elisp-packages
-  (package-input-rewriting
-   `((,e:emacs-f . ,emacs-f)
-     (,e:emacs-pkg-info . ,emacs-pkg-info)
-     (,e:emacs-dash . ,emacs-dash)
-     (,e:emacs-hydra . ,emacs-hydra)
-     (,e:emacs-flycheck . ,emacs-flycheck)
-     (,emacs-lsp-mode . ,e:emacs-lsp-mode)
-     (,e:emacs-markdown-mode . ,emacs-markdown-mode)
-     (,emacs-emacsql . ,e:emacs-emacsql))))
+  (package-input-rewriting/spec
+   `(("emacs-f" . ,(const emacs-f))
+     ("emacs-s" . ,(const emacs-s))
+     ("emacs-pkg-info" . ,(const emacs-pkg-info))
+     ("emacs-dash" . ,(const emacs-dash))
+     ("emacs-hydra" . ,(const emacs-hydra))
+     ("emacs-flycheck" . ,(const emacs-flycheck))
+     ("emacs-lsp-mode" . ,(const emacs-lsp-mode))
+     ("emacs-markdown-mode" . ,(const emacs-markdown-mode))
+     ("emacs-emacsql" . ,(const emacs-emacsql))
+     ("emacs-yasnippet" . ,(const emacs-yasnippet))
+     ("emacs-cider" . ,(const emacs-cider))
+     ("emacs-org-contrib" . ,(const emacs-org-contrib)))))
+
+(define %native-emacs emacs-native-comp)
+
+(define (update-emacs-argument-for-package p)
+  (if (equal?
+       (package-build-system p)
+       melpa-build-system)
+      (package (inherit p)
+	       (arguments
+		(substitute-keyword-arguments (package-arguments p)
+					      ((#:emacs e #f) %native-emacs))))
+      p))
+
+(define use-native-emacs
+  (package-mapping update-emacs-argument-for-package
+		   (lambda (p) #f)))
 
 (define %elisp-packages
   (map rewrite-elisp-packages
-       (list
-	;; emacs-doom-modeline
-	emacs-helpful
-	emacs-use-package
-	emacs-which-key
-	emacs-gcmh
-	e:emacs-hl-todo
-	emacs-git-gutter
-	emacs-git-gutter-fringe
-	emacs-exec-path-from-shell
-	emacs-vertico
-	emacs-marginalia
-	emacs-orderless
-	emacs-consult
-	emacs-embark
-	;; emacs-embark-consult
+       (map use-native-emacs
+	    (list
+	     ;; emacs-doom-modeline
+	     emacs-helpful
+	     emacs-use-package
+	     emacs-which-key
+	     emacs-gcmh
+	     emacs-hl-todo
+	     emacs-git-gutter
+	     emacs-git-gutter-fringe
+	     emacs-exec-path-from-shell
+	     emacs-vertico
+	     emacs-marginalia
+	     emacs-orderless
+	     emacs-consult
+	     emacs-embark
+	     emacs-embark-consult
 
-	emacs-projectile
+	     emacs-projectile
 
-	;; emacs-evil
-	;; emacs-evil-collection
-	;; emacs-evil-surround
-	;; emacs-evil-commentary
-	emacs-general
-	emacs-undo-fu
-	emacs-undo-fu-session
-	emacs-eldoc
+	     ;; emacs-evil
+	     ;; emacs-evil-collection
+	     ;; emacs-evil-surround
+	     ;; emacs-evil-commentary
+	     emacs-general
+	     emacs-undo-fu
+	     emacs-undo-fu-session
+	     emacs-eldoc
 
-	e:emacs-magit
-	e:emacs-forge
+	     emacs-magit
+	     emacs-forge
 
-	emacs-diredfl
-	emacs-all-the-icons-dired
-	emacs-diff-hl
+	     emacs-diredfl
+	     emacs-all-the-icons-dired
+	     emacs-diff-hl
 
-	emacs-sly
-	;; emacs-sly-macrostep
-	;; emacs-sly-repl-ansi-color
+	     emacs-sly
+	     emacs-sly-macrostep
+	     emacs-sly-repl-ansi-color
 
-	emacs-lispy
-	;; emacs-lispyville
+	     emacs-lispy
+	     ;; emacs-lispyville
 
-	emacs-ccls
-	emacs-cmake-mode
-	emacs-demangle-mode
-	emacs-disaster
-	emacs-modern-cpp-font-lock
+	     emacs-ccls
+	     emacs-cmake-mode
+	     emacs-demangle-mode
+	     emacs-disaster
+	     emacs-modern-cpp-font-lock
 
-	emacs-company
-	emacs-company-box
+	     emacs-company
+	     emacs-company-box
 
-	emacs-csharp-mode
+	     emacs-csharp-mode
 
-	emacs-yaml-mode
-	emacs-gitlab-ci-mode
+	     emacs-yaml-mode
+	     emacs-gitlab-ci-mode
 
-	emacs-envrc
+	     emacs-envrc
 
-	emacs-eros
-	emacs-highlight-quoted
-	emacs-elisp-demos
-	emacs-macrostep
+	     emacs-eros
+	     emacs-highlight-quoted
+	     emacs-elisp-demos
+	     emacs-macrostep
 
-	emacs-elixir-mode
-	emacs-alchemist
-	emacs-exunit
+	     ;; emacs-elixir-mode
+	     ;; emacs-alchemist
+	     ;; emacs-exunit
 
-	emacs-fish-mode
+	     emacs-fish-mode
 
-	emacs-flycheck
-	emacs-flycheck-inline
+	     emacs-flycheck
+	     emacs-flycheck-inline
 
-	;; emacs-format-all
+	     ;; emacs-format-all
 
-	emacs-go-mode
-	emacs-go-guru
+	     emacs-go-mode
+	     emacs-go-guru
 
-	e:emacs-lsp-mode
-	e:emacs-lsp-ui
-	;; emacs-consult-lsp
+	     emacs-lsp-mode
+	     emacs-lsp-ui
+	     emacs-consult-lsp
 
-	emacs-org
-	emacs-org-appear
-	emacs-org-contrib
-	emacs-org-edna
-	emacs-org-superstar
-	;; emacs-evil-org
-	emacs-org-roam
+	     emacs-org
+	     emacs-org-appear
+	     emacs-org-contrib
+	     emacs-org-edna
+	     emacs-org-superstar
+	     ;; emacs-evil-org
+	     emacs-org-roam
 
-	;; emacs-nix-mode
-	;; emacs-nix-update
+	     emacs-nix-mode
+	     emacs-nix-update
 
-	;; emacs-perspective
-	;; emacs-persp-projectile
+	     ;; emacs-perspective
+	     ;; emacs-persp-projectile
 
-	;; emacs-python-mode
-	;; emacs-poetry
-	;; emacs-lsp-python-ms
+	     emacs-python-mode
+	     emacs-poetry
+	     emacs-lsp-python-ms
 
-	emacs-smartparens
+	     emacs-smartparens
 
-	e:emacs-yasnippet
-	emacs-doom-snippets
+	     emacs-yasnippet
+	     emacs-doom-snippets
 
-	emacs-doom-themes
-	emacs-kaolin-themes
-	emacs-modus-themes
-	emacs-inkpot-theme
+	     emacs-doom-themes
+	     emacs-kaolin-themes
+	     emacs-modus-themes
+	     emacs-inkpot-theme
 
-	emacs-vterm
+	     emacs-vterm
 
-	e:emacs-geiser
-	e:emacs-guix
+	     emacs-geiser
+	     emacs-guix
 
-	emacs-emacsql-sqlite3
-	sqlite
-	gcc
-	emacs-editorconfig
-	emacs-git-auto-commit-mode
-	emacs-zoom
-	emacs-pinentry
-	emacs-crux
-	emacs-htmlize
+	     emacs-emacsql-sqlite3
+	     sqlite
+	     gcc
+	     emacs-editorconfig
+	     emacs-git-auto-commit-mode
+	     emacs-zoom
+	     ;; emacs-pinentry
+	     emacs-crux
+	     emacs-htmlize
 
-	emacs-avy
-	emacs-ace-window
-	)))
+	     emacs-avy
+	     emacs-ace-window
+	     ))))
 
 ;; TODO: migrate this to a ~home-config-files-service-type~
 (define (emacs-files)
@@ -183,10 +206,10 @@
 (define emacs-service
   (list (service home-emacs-service-type
 		 (home-emacs-configuration
-		  (package emacs-pgtk-native-comp)
+		  (package %native-emacs)
 		  (elisp-packages %elisp-packages)
 		  (server-mode? #t)
-		  (rebuild-elisp-packages? #f)
+		  (rebuild-elisp-packages? #t)
 		  (xdg-flavor? #t)))
 	(simple-service 'emacs-init
 			home-files-service-type
