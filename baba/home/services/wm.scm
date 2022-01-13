1d57ce251fd88fe50a3f6b786f4d231a02362331
Bastien Riviere <babathriviere@gmail.com>
2022-01-13 14:50:55 +0100

    chore(stumpwm): add necessary tools

--- baba/home/services/wm.scm
+++ baba/home/services/wm.scm
@@ -4,7 +4,9 @@
   #:use-module (gnu packages compton)
   #:use-module (gnu packages lisp)
   #:use-module (gnu packages lisp-xyz)
+  #:use-module (gnu packages terminals)
   #:use-module (gnu packages wm)
+  #:use-module (gnu packages xdisorg)
   #:use-module (gnu services)
   #:use-module (guix gexp)
   #:export (stumpwm-service))
@@ -15,41 +17,46 @@
   (list
    ;; NOTE: possible with .xsession since it's used by default with slim
    (simple-service 'stumpwm-xsession
-		   home-files-service-type
-		   `(("xprofile" ;; NOTE: move it into a service? to be able to extend it
-		      ,(computed-file
-			"xprofile"
-			#~(begin
-			    (use-modules (ice-9 format))
-			    (with-output-to-file #$output
-			      (lambda ()
-				(format #t
-					"#!/bin/sh~@
+		           home-files-service-type
+		           `(("xprofile" ;; NOTE: move it into a service? to be able to extend it
+		              ,(computed-file
+			            "xprofile"
+			            #~(begin
+			                (use-modules (ice-9 format))
+			                (with-output-to-file #$output
+			                  (lambda ()
+				                (format #t
+					                    "#!/bin/sh~@
                                          if [ -e \"$HOME/.profile\" ]; then~@
-                                           . \"$HOME/.profile\"~@
+                                         . \"$HOME/.profile\"~@
                                          fi~@
                                          ~a/bin/picom -b --config $HOME/.config/picom.conf"
-					#$picom)))
-			    (chmod #$output #o555))))
-		     ("xsession"
-		      ,(computed-file
-			"xsession"
-			#~(begin
-			    (use-modules (ice-9 format))
-			    (with-output-to-file #$output
-			      (lambda ()
-				(format #t
-					"#!/bin/sh~@
+					                    #$picom)))
+			                (chmod #$output #o555))))
+		             ("xsession"
+		              ,(computed-file
+			            "xsession"
+			            #~(begin
+			                (use-modules (ice-9 format))
+			                (with-output-to-file #$output
+			                  (lambda ()
+				                (format #t
+					                    "#!/bin/sh~@
                                          ~a/bin/stumpwm"
-					#$stump)))
-			    (chmod #$output #o555))))))
+					                    #$stump)))
+			                (chmod #$output #o555))))))
    (simple-service 'stumpwm-profile
-		   home-profile-service-type
-		   (list stump `(,stumpwm "lib")
-			 sbcl-stumpwm-ttf-fonts
-			 sbcl))
+		           home-profile-service-type
+		           (list stump `(,stumpwm "lib")
+			             sbcl-stumpwm-ttf-fonts
+			             sbcl
+
+                         ;; tools
+                         alacritty
+                         autorandr
+                         picom))
    (simple-service 'stumpwm-files
-		   home-files-service-type
-		   `(("stumpwm.d/init.lisp"
-		      ,(local-file
-			(string-append %channel-root "/etc/stumpwm.d/init.lisp")))))))
+		           home-files-service-type
+		           `(("stumpwm.d/init.lisp"
+		              ,(local-file
+			            (string-append %channel-root "/etc/stumpwm.d/init.lisp")))))))
