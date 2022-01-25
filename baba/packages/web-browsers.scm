957cd5bda9fb2c2b050aa0070c6d65b212fe6c16
Bastien Riviere <babathriviere@gmail.com>
2022-01-25 11:58:37 +0100

    nyxt: bump to try context sharing and seperation

--- baba/packages/web-browsers.scm
+++ baba/packages/web-browsers.scm
@@ -1,9 +1,12 @@
 (define-module (baba packages web-browsers)
   #:use-module (gnu packages gstreamer)
   #:use-module (gnu packages lisp-xyz)
+  #:use-module (gnu packages tls)
   #:use-module (gnu packages web-browsers)
   #:use-module (gnu packages webkit)
+  #:use-module (guix build-system asdf)
   #:use-module (guix git-download)
+  #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix packages)
   #:use-module (guix utils))
 
@@ -16,11 +19,13 @@
      (method git-fetch)
      (uri (git-reference
            (url "https://github.com/atlas-engineer/nyxt")
-           (commit "bdf8e4cdf9628a349e5c618016c5b06cd14b96e6")))
+           (commit "2d678d04bd9bb45a80f6caf2ca1056719698313e")))
      (sha256
       (base32
-       "1hw98vysi3gl7pc6lfj54h6zr6rgbn89id8ab390nhid8nrffjbz"))
+       "0pqc4sh8ff1kb7jszk6l710h4rdna0acyk466rd09fy8vf2rhgrv"))
      (file-name (git-file-name "nyxt" version))))
    (inputs
     `(("cl-gopher" ,sbcl-cl-gopher)
+      ("nhooks" ,sbcl-nhooks)
+      ("phos" ,sbcl-phos)
       ,@(package-inputs nyxt)))))
