44e24c175620b94047e4403a9d8169af7349b99c
Bastien Riviere <babathriviere@gmail.com>
2022-02-06 20:13:27 +0100

    keychain: combine two calls

--- etc/fish/config.fish
+++ etc/fish/config.fish
@@ -1,6 +1,5 @@
 direnv hook fish | source
 
 if status --is-interactive
-    keychain --eval --quiet -Q id_ed25519 | source
-    keychain --eval --quiet -Q --agents gpg | source
+    keychain --eval --quiet -Q --agents ssh,gpg id_ed25519 | source
 end
