fa57ee2b9320971f14cded48891ef04fab884808
Bastien Riviere <babathriviere@gmail.com>
2022-03-10 21:38:19 +0100

    keychain: split into two command

--- etc/fish/config.fish
+++ etc/fish/config.fish
@@ -1,5 +1,6 @@
 direnv hook fish | source
 
 if status --is-interactive
-    keychain --eval --quiet -Q --agents ssh,gpg id_ed25519 | source
+    keychain --eval --quiet -Q --agents ssh id_ed25519 | source
+    keychain --eval --quiet -Q --agents gpg | source
 end
