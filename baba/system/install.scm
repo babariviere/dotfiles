d6f851231aa73ba828a99b7cebe3c943f12d641a
Bastien Riviere <babathriviere@gmail.com>
2022-03-26 21:21:36 +0100

    install: enforce latest bcachefs + linux-bcachefs

--- baba/system/install.scm
+++ baba/system/install.scm
@@ -1,15 +1,22 @@
 (define-module (baba system install)
+  #:use-module (baba packages linux)
   #:use-module (baba packages file-systems)
   #:use-module (gnu system)
   #:use-module (gnu system install)
+  #:use-module (guix packages)
   #:use-module (nongnu packages linux)
   #:export (installation-os-nonfree))
 
+(define replace-bcachefs
+  (package-input-rewriting/spec
+   `(("bcachefs-tools" . ,(const bcachefs-tools-git))
+     ("bcachefs" . ,(const bcachefs-git/static)))))
+
 (define installation-os-nonfree
   (operating-system
-   (inherit installation-os)
-   (kernel linux)
-   (firmware (list linux-firmware))
-   (packages (cons* bcachefs-tools-git (operating-system-packages installation-os)))))
+    (inherit installation-os)
+    (kernel linux-bcachefs)
+    (firmware (list linux-firmware))
+    (packages (cons* bcachefs-tools-git (map replace-bcachefs (operating-system-packages installation-os))))))
 
 installation-os-nonfree
