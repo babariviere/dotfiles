ab27c066c9c29baa78b826f6b97d4978c364c3dc
Bastien Riviere <babathriviere@gmail.com>
2022-03-28 14:04:45 +0200

    nyxt: add slynk

--- etc/nyxt/init.lisp
+++ etc/nyxt/init.lisp
@@ -1,3 +1,9 @@
+;; Local Variables:
+;; sly-port: 4006
+;; End:
+
+(require :slynk)
+
 ;; TODO: use https://github.com/aartaka/nx-search-engines
 (defvar *search-engines*
   (list '("spg" "https://startpage.com/do/search?query=~a" "https://startpage.com"))
@@ -10,3 +16,14 @@
   ((default-modes (append '(emacs-mode) %slot-default%))
    (search-engines (mapcar (lambda (engine) (apply 'make-search-engine engine))
                            *search-engines*))))
+
+(define-command-global start-slynk (&optional (slynk-port *swank-port*))
+  "Start a Slynk server that can be connected to, for instance, in
+    Emacs via SLY.
+
+    Warning: This allows Nyxt to be controlled remotely, that is, to execute
+    arbitrary code with the privileges of the user running Nyxt.  Make sure
+    you understand the security risks associated with this before running
+    this command."
+  (slynk:create-server :port slynk-port :dont-close t)
+  (echo "Slynk server started at port ~a" slynk-port))
