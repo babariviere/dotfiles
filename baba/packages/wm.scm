90ce8071a8a6f1e7f4b46e12cfab10742d7159a6
Bastien Riviere <babathriviere@gmail.com>
2022-04-20 17:38:32 +0200

    stumpwm: bump to master and add prescient package

--- baba/packages/wm.scm
+++ baba/packages/wm.scm
@@ -1,11 +1,34 @@
 (define-module (baba packages wm)
   #:use-module (gnu packages lisp)
+  #:use-module (gnu packages lisp-check)
   #:use-module (gnu packages lisp-xyz)
   #:use-module (gnu packages wm)
   #:use-module (guix build-system asdf)
   #:use-module (guix git-download)
   #:use-module ((guix licenses) #:prefix license:)
-  #:use-module (guix packages))
+  #:use-module (guix packages)
+  #:use-module (guix utils))
+
+(define-public stumpwm-next
+  (let ((commit "9bfb9686e1185ec676489e610108ea50c12fd513")
+        (revision "1"))
+    (package
+     (inherit stumpwm)
+     (name "stumpwm")
+     (version (git-version "20.11" revision commit))
+     (source
+      (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/stumpwm/stumpwm")
+             (commit commit)))
+       (sha256
+        (base32 "1a950925f7314ajk7xcw1jwlfgg82zi0mnzb3sv3mp94jj1kkj5l"))
+       (file-name (git-file-name name version))))
+     (arguments
+      (substitute-keyword-arguments (package-arguments stumpwm)
+                                    ((#:tests? tests #f)
+                                     #f))))))
 
 (define-public sbcl-stumpwm-battery-portable
   (package
@@ -69,3 +92,30 @@ sorts of useful XML output; it has an RSS 2.0 emitter built in.")
    (synopsis "DBus-based notification server part.")
    (description "Notification support for stumpwm.")
    (license license:gpl3+)))
+
+(define-public sbcl-stumpwm-prescient
+  (let ((commit "15f40d3d0fa39d8d9ca2dcae0bc3f41c5c4763ed"))
+    (package
+     (name "sbcl-stumpwm-prescient")
+     (version (git-version "0" "1" commit))
+     (source
+      (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/landakram/stumpwm-prescient")
+             (commit commit)))
+       (sha256
+        (base32 "0ir3r3nvf61p77i9pkhfjlwnry39km20g325bi231cl6bhmqrnrl"))
+       (file-name (git-file-name name version))))
+     (build-system asdf-build-system/sbcl)
+     (inputs
+      `((,stumpwm-next "lib")))
+     (native-inputs
+      (list sbcl-rove))
+     (arguments
+      '(#:asd-systems '("stumpwm-prescient" "stumpwm-prescient/tests")
+        #:tests? #f))
+     (home-page "https://github.com/landakram/stumpwm-prescient")
+     (synopsis "Prescient for stumpwm. Simple but effective sorting and filtering for stumpwm inputs.")
+     (description "Prescient for stumpwm. Simple but effective sorting and filtering for stumpwm inputs.")
+     (license #f))))
