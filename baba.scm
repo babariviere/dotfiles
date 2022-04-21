0efdabeee122709c5ab19d04a162bcd2f014f5b3
Bastien Riviere <babathriviere@gmail.com>
2022-04-22 01:05:07 +0200

    chore: add etc-file helper

--- baba.scm
+++ baba.scm
@@ -1,6 +1,6 @@
 (define-module (baba)
   #:use-module (srfi srfi-1)
-  #:export (%channel-root))
+  #:export (%channel-root etc-file))
 
 (define %channel-root
   (canonicalize-path
@@ -10,3 +10,6 @@
 	    (file-exists? (string-append path "/baba"))
 	    (file-exists? (string-append path "/baba.scm"))))
 	 %load-path)))
+
+(define (etc-file path)
+  (string-append %channel-root path))
