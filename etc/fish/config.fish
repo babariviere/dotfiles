e489281d2ce271676d38a25ec6938c9f959953eb
Bastien Riviere <babathriviere@gmail.com>
2022-04-26 18:45:14 +0200

    fish: set gpg tty only when interactive

--- etc/fish/config.fish
+++ etc/fish/config.fish
@@ -1,5 +1,8 @@
 direnv hook fish | source
 
-set -x GPG_TTY (tty)
-set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
-gpgconf --launch gpg-agent
+status --is-interactive;
+and begin
+    set -x GPG_TTY (tty)
+    set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+    gpgconf --launch gpg-agent
+end
