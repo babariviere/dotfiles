8494f7d84f6abb980f79d3dd8eb2b68dc28c6663
Bastien Riviere <me@babariviere.com>
2022-07-10 12:14:08 +0200

    home: xmobar: fix typo in filename

--- home/profiles/desktop/xmobar.nix
+++ home/profiles/desktop/xmobar.nix
@@ -8,7 +8,7 @@
   xdg.configFile = {
     "xmobar/xmobarrc0".source = "${config.dotfiles.configDir}/xmobar/xmobarrc0";
     "xmobar/xmobarrc1".source = "${config.dotfiles.configDir}/xmobar/xmobarrc1";
-    "xmobar/trayer-padding.hs" = {
+    "xmobar/trayer-padding.sh" = {
       source = "${config.dotfiles.configDir}/xmobar/trayer-padding.sh";
       executable = true;
     };
