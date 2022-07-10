6280f8ed09485b7abb33379f98c17e80479642f8
Bastien Riviere <me@babariviere.com>
2022-07-10 12:08:37 +0200

    system: nvidia: enforce dpi and blacklist nouveau

--- system/profiles/desktop/nvidia.nix
+++ system/profiles/desktop/nvidia.nix
@@ -1,7 +1,11 @@
 { config, pkgs }:
 
 {
+  boot.blacklistedKernelModules = [ "nouveau" ];
+
+  services.xserver.dpi = 96;
+
   hardware.opengl.enable = true;
   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
-  services.xserver.videoDrivers = [ "nvidia" "modesetting" ];
+  services.xserver.videoDrivers = [ "nvidia" ];
 }
