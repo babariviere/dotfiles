7a87d47cb8c1db1611294809e457f2fbf01e36b3
Bastien Riviere <me@babariviere.com>
2022-07-11 10:00:03 +0200

    home: xmonad: add feh

--- home/profiles/desktop/xmonad.nix
+++ home/profiles/desktop/xmonad.nix
@@ -7,7 +7,7 @@
     xmobar.enable = true;
   };
 
-  home.packages = with pkgs; [ rofi biosevka ];
+  home.packages = with pkgs; [ rofi biosevka feh ];
 
   xdg.configFile."xmonad/xmonad.hs".source = config.lib.file.mkOutOfStoreSymlink
     "${config.dotfiles.configDir}/xmonad/xmonad.hs";
