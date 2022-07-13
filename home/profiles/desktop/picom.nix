bb6810873a59d225ff71fdca76c6a2a8fc28da05
Bastien Riviere <me@babariviere.com>
2022-07-13 11:32:03 +0200

    home: picom: use picom-next

--- home/profiles/desktop/picom.nix
+++ home/profiles/desktop/picom.nix
@@ -1,10 +1,13 @@
+{ pkgs }:
+
 {
   services.picom = {
     enable = true;
+    package = pkgs.picom-next;
     backend = "glx";
     vSync = true;
     extraOptions = ''
-      unredir-if-possible = true;
+      unredir-if-possible = false;
     '';
   };
 }
